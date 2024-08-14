7d16d5fbe0c60e704739d2f97c1738e07b3a45bf
joetaylorland <accounts+github@joetaylor.net>
2024-08-14 08:29:24 -0700

    mapped out basic skeleton

--- packages/playground/src/pages/pipeline-list-page.tsx
+++ packages/playground/src/pages/pipeline-list-page.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import PipelineList from './pull-request-list-page'
 
-const mockPipelines = [
-  { id: '1', name: 'Pipeline One', description: 'This is the first pipeline.' },
-  { id: '2', name: 'Pipeline Two', description: 'This is the second pipeline.' },
-  { id: '3', name: 'Pipeline Three', description: 'This is the third pipeline.' }
-]
-
-const PipelineList: React.FC = () => {
+export default function PipelineListPage() {
   return (
-    <div>
-      <ul>
-        {mockPipelines.map(pipeline => (
-          <li key={pipeline.id}>
-            <Link to={pipeline.id}>Pipeline {pipeline.id}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Outlet />
+      <PipelineList />
+    </>
   )
 }
-
-export default PipelineList

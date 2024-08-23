0cfee904ff5df74d00d94101f4ce65640ec7dc3d
jessieyu777 <c_peipeijessie@harness.io>
2024-08-22 17:05:24 -0700

    create reusable layout components for wrapping each list-page: repo-ex-list, repo-pip-list, ex-list, pip-list

--- packages/playground/src/layouts/PaddingListLayout.tsx
+++ packages/playground/src/layouts/PaddingListLayout.tsx
@@ -3,10 +3,11 @@ import { cn } from '@harnessio/canary'
 
 interface PaddingListLayoutProps {
   className?: string
+  children: React.ReactNode
 }
 
-const PaddingListLayout: React.FC<PaddingListLayoutProps> = ({ className }) => {
-  return <div className={cn('p-10', className)}></div>
+const PaddingListLayout = ({ className, children }: PaddingListLayoutProps) => {
+  return <div className={cn('pt-10 px-6 pb-16 max-w-[1200px] min-w-[770px] mx-auto', className)}>{children}</div>
 }
 
 export default PaddingListLayout

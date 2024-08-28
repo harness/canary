105aa4b620712bf3d072cb568e00467cd9719839
joetaylorland <accounts+github@joetaylor.net>
2024-08-28 17:24:16 +0100

    corrected placeholder text

--- packages/playground/src/components/pull-request/pull-request-comment-box.tsx
+++ packages/playground/src/components/pull-request/pull-request-comment-box.tsx
@@ -5,7 +5,7 @@ const PullRequestCommentBox: React.FC = () => {
   return (
     <div className="flex gap-3 items-start">
       <div className='h-6 w-6 rounded-full bg-tertiary-background bg-[url("../images/user-avatar.svg")] bg-cover'></div>
-      <Textarea placeholder={'Reply here'} className="h-28" />
+      <Textarea placeholder={'Add your comment here'} className="h-28" />
     </div>
   )
 }

70fffcd726a2aac1fe74fdb3d352f32eff9e2c67
joetaylorland <accounts+github@joetaylor.net>
2024-08-28 14:20:11 +0100

    componentized pr sidebar

--- packages/playground/src/components/pull-request/pull-request-side-bar.tsx
+++ packages/playground/src/components/pull-request/pull-request-side-bar.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import cx from 'classnames'
 import { CheckCircleSolid, WarningTriangleSolid, Clock, ChatBubbleQuestionSolid } from '@harnessio/icons-noir'
 import { Button, Avatar, AvatarFallback, Icon, Text } from '@harnessio/canary'
-import { EnumPullReqReviewDecision, PullReqReviewDecision, ReviewerData } from './interfaces'
+import { EnumPullReqReviewDecision, PullReqReviewDecision } from './interfaces'
 import { getInitials } from '../../utils/utils'
 
 interface PullRequestSideBarProps {
-  reviewers?: ReviewerData[]
+  reviewers?: {
+    reviewer: { display_name: string; id: number }
+    review_decision: EnumPullReqReviewDecision
+    sha: string
+  }[]
   processReviewDecision: (
     review_decision: EnumPullReqReviewDecision,
     reviewedSHA?: string,
@@ -17,77 +21,81 @@ interface PullRequestSideBarProps {
 }
 
 const PullRequestSideBar = (props: PullRequestSideBarProps) => {
-  const { reviewers, pullRequestMetadata, processReviewDecision } = props
-  // TODO: add toaster error message
-  //   const { showError } = useToaster()
+  const { reviewers = [], pullRequestMetadata, processReviewDecision, refetchReviewers } = props
+
+  const ReviewerItem = ({
+    reviewer,
+    reviewDecision,
+    sha,
+    sourceSHA
+  }: {
+    reviewer: { display_name: string; id: number }
+    reviewDecision: EnumPullReqReviewDecision
+    sha: string
+    sourceSHA?: string
+  }) => {
+    const updatedReviewDecision = processReviewDecision(reviewDecision, sha, sourceSHA)
+
+    return (
+      <div key={reviewer.id} className="flex items-center space-x-2 mr-1">
+        <Avatar
+          className={cx('w-7 h-7 rounded-full', {
+            'p-0': updatedReviewDecision !== PullReqReviewDecision.changeReq
+          })}>
+          <AvatarFallback>
+            <span className="text-xs">{getInitials(reviewer.display_name)}</span>
+          </AvatarFallback>
+        </Avatar>
+        <div className="truncate reviewerName">{reviewer.display_name}</div>
+        <div className="flex-grow"></div>
+
+        {updatedReviewDecision === PullReqReviewDecision.outdated ? (
+          <ChatBubbleQuestionSolid className="text-warning" />
+        ) : updatedReviewDecision === PullReqReviewDecision.approved ? (
+          <CheckCircleSolid className="text-success" />
+        ) : updatedReviewDecision === PullReqReviewDecision.changeReq ? (
+          <WarningTriangleSolid className="text-destructive" />
+        ) : updatedReviewDecision === PullReqReviewDecision.pending ? (
+          <Clock />
+        ) : null}
+      </div>
+    )
+  }
+
+  const ReviewersList = () => (
+    <div className="flex flex-col gap-3">
+      {reviewers.length ? (
+        reviewers.map(({ reviewer, review_decision, sha }) => (
+          <ReviewerItem
+            key={reviewer.id}
+            reviewer={reviewer}
+            reviewDecision={review_decision}
+            sha={sha}
+            sourceSHA={pullRequestMetadata?.source_sha}
+          />
+        ))
+      ) : (
+        <div className="text-tertiary-background text-sm font-[500]">No reviewers</div>
+      )}
+    </div>
+  )
+
+  const ReviewersHeader = () => (
+    <div className="flex justify-between items-center">
+      <Text size={2} weight="medium">
+        Reviewers
+      </Text>
+      <Button size="sm" variant="ghost" className="px-2 py-1" onClick={refetchReviewers}>
+        <Icon name="vertical-ellipsis" size={12} />
+      </Button>
+    </div>
+  )
+
   return (
     <div>
-      <div className="flex flex-col">
-        <div className="flex items-center">
-          <Text size={2} weight="medium">
-            Reviewers
-          </Text>
-          <div className="flex-grow"></div>
-          {/* TODO: how to handle dropdown or add new button */}
-          {/* <ReviewerSelect
-              pullRequestMetadata={pullRequestMetadata}
-              onSelect={function (id: number): void {
-                updateCodeCommentStatus({ reviewer_id: id }).catch(err => {
-                  showError(getErrorMessage(err))
-                })
-                if (refetchReviewers) {
-                  refetchReviewers()
-                }
-              }}
-            /> */}
-          <Button size="sm" variant="ghost" className="px-2 py-1">
-            <Icon name="vertical-ellipsis" size={12} />
-          </Button>
-        </div>
-        <div className="pt-2 pb-4 flex flex-col gap-3">
-          {reviewers && reviewers.length !== 0 && reviewers !== null ? (
-            reviewers.map(
-              (reviewer: {
-                reviewer: { display_name: string; id: number }
-                review_decision: EnumPullReqReviewDecision
-                sha: string
-              }) => {
-                const updatedReviewDecision = processReviewDecision(
-                  reviewer.review_decision,
-                  reviewer.sha,
-                  pullRequestMetadata?.source_sha
-                )
-
-                return (
-                  <div key={reviewer.reviewer.id} className="flex items-center space-x-2 mr-1">
-                    <Avatar
-                      className={cx('w-7 h-7 rounded-full', {
-                        'p-0': updatedReviewDecision !== PullReqReviewDecision.changeReq
-                      })}>
-                      <AvatarFallback>
-                        <span className="text-xs"> {getInitials(reviewer.reviewer.display_name)}</span>
-                      </AvatarFallback>
-                    </Avatar>
-                    <div className="truncate reviewerName">{reviewer.reviewer.display_name}</div>
-                    <div className="flex-grow"></div>
-
-                    {updatedReviewDecision === PullReqReviewDecision.outdated ? (
-                      <ChatBubbleQuestionSolid className="text-warning" />
-                    ) : updatedReviewDecision === PullReqReviewDecision.approved ? (
-                      <CheckCircleSolid className="text-success" />
-                    ) : updatedReviewDecision === PullReqReviewDecision.changeReq ? (
-                      <WarningTriangleSolid className="text-destructive" />
-                    ) : updatedReviewDecision === PullReqReviewDecision.pending ? (
-                      <Clock />
-                    ) : null}
-                  </div>
-                )
-              }
-            )
-          ) : (
-            <div className="text-tertiary-background text-sm font-[500]">No reviewers</div>
-          )}
-        </div>
+      <div className="flex flex-col gap-3">
+        <ReviewersHeader />
+        <ReviewersList />
       </div>
     </div>
   )

38bbbb94f04eab9da9a709b1affc8276f9799886
joetaylorland <accounts+github@joetaylor.net>
2024-08-29 16:25:23 +0100

    componentize pr changes page

--- packages/playground/src/pages/pull-request-changes-page.tsx
+++ packages/playground/src/pages/pull-request-changes-page.tsx
@@ -2,27 +2,8 @@ import React, { useState } from 'react'
 import PlaygroundPullRequestChangesSettings from '../components/playground/pull-request-changes-settings'
 import SkeletonList from '../components/loaders/skeleton-list'
 import NoData from '../components/no-data'
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  Text,
-  ListActions,
-  Spacer,
-  SplitButton,
-  StackedList,
-  AccordionContent
-} from '@harnessio/canary'
-
-interface LineTitleProps {
-  text?: string
-}
-
-const LineTitle: React.FC<LineTitleProps> = ({ text }) => (
-  <div className="inline-flex gap-2 items-center">
-    <Text weight="medium">{text}</Text>
-  </div>
-)
+import { ListActions, Spacer, SplitButton } from '@harnessio/canary'
+import PullRequestChanges from '../components/pull-request/pull-request-changes'
 
 const FilterSortViewDropdowns: React.FC = () => {
   const filterOptions = [{ name: 'Filter option 1' }, { name: 'Filter option 2' }, { name: 'Filter option 3' }]
@@ -44,24 +25,6 @@ const FilterSortViewDropdowns: React.FC = () => {
     </ListActions.Root>
   )
 }
-
-const PullRequestAccordion: React.FC<{ title: string }> = ({ title }) => (
-  <StackedList.Root>
-    <StackedList.Item isHeader className="p-0 hover:bg-transparent cursor-default">
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem isLast value={title}>
-          <AccordionTrigger leftChevron className="text-left p-4">
-            <AccordionItem isLast value={title}></AccordionItem>
-            <StackedList.Field title={<LineTitle text={title} />} />
-          </AccordionTrigger>
-          <AccordionContent>
-            <div className="flex border-t w-full h-32 p-4"></div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </StackedList.Item>
-  </StackedList.Root>
-)
 
 export default function PullRequestChangesPage() {
   const [loadState, setLoadState] = useState('data-loaded') // Change to data-loaded when component work is finished
@@ -97,11 +60,7 @@ export default function PullRequestChangesPage() {
       <>
         <FilterSortViewDropdowns />
         <Spacer aria-setsize={5} />
-        <div className="flex flex-col gap-4">
-          {pullRequestData.map((item, index) => (
-            <PullRequestAccordion key={index} title={item} />
-          ))}
-        </div>
+        <PullRequestChanges data={pullRequestData} />
         <Spacer size={5} />
         <PlaygroundPullRequestChangesSettings loadState={loadState} setLoadState={setLoadState} />
       </>

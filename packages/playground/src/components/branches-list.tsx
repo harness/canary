286fc7448a17041f1bceaf73de3226d7f3045496
jessieyu777 <c_peipeijessie@harness.io>
2024-10-03 16:33:55 -0700

    delete branch # only show sha

--- packages/playground/src/components/branches-list.tsx
+++ packages/playground/src/components/branches-list.tsx
@@ -128,7 +128,7 @@ export const BranchesList = ({ branches }: PageProps) => {
                     <div className="flex gap-1.5 items-center justify-center">
                       {/* <Icon name="open-pr" size={11} className="text-success" /> */}
                       <Text wrap="nowrap" size={1} truncate className="text-tertiary-background">
-                        #{shortSha}
+                        {shortSha}
                       </Text>
                     </div>
                   </TableCell>

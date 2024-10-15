0e74c48eea63366a1a3f2fd82737801aeb0b5949
sanskar <c_sanskar.sehgal@harness.io>
2024-10-15 11:31:20 -0700

    eslint fixes

--- apps/gitness/src/pages/repo-sandbox/repo-sandbox-header.tsx
+++ apps/gitness/src/pages/repo-sandbox/repo-sandbox-header.tsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { SandboxLayout } from '@harnessio/playground'
-import { Topbar, Text } from '@harnessio/canary'
 import { TopBarWidget, Project } from '@harnessio/playground'
 import { useNavigate } from 'react-router-dom'
-import { TypesMembershipSpace } from '@harnessio/code-service-client'
 import { useAppContext } from '../../framework/context/AppContext'
 import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
 

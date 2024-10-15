1923033cf4278d28db964e529596477b429e0d04
sanskar <c_sanskar.sehgal@harness.io>
2024-10-15 15:08:01 -0700

    fix eslint issues

--- packages/canary/src/utils/StringUtils.ts
+++ packages/canary/src/utils/StringUtils.ts
@@ -10,13 +10,3 @@ export const getInitials = (name: string, length?: number) => {
   // If length is provided, truncate the initials to the desired length
   return length ? initials.slice(0, length) : initials
 }
-export const INITIAL_ZOOM_LEVEL = 1
-export const ZOOM_INC_DEC_LEVEL = 0.1
-
-const LOCALE = Intl.NumberFormat().resolvedOptions?.().locale || 'en-US'
-
-/**
- * Format a timestamp to medium format date (i.e: Jan 1, 2021)
- * @param timestamp Timestamp
- * @param dateStyle Optional DateTimeFormat's `dateStyle` option.
- */

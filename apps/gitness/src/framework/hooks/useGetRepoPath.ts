fd2b21edc110354f0b99f87320238e58b06015b0
sanskar <c_sanskar.sehgal@harness.io>
2024-10-25 19:44:44 -0700

    minor fixes

--- apps/gitness/src/framework/hooks/useGetRepoPath.ts
+++ apps/gitness/src/framework/hooks/useGetRepoPath.ts
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 import { PathParams } from '../../RouteDefinitions'
 import { useGetSpaceURLParam } from './useGetSpaceParam'
 
-export function useGetRepoRef() {
+export function useGetRepoRef(): string {
   const space = useGetSpaceURLParam()
   const { repoId } = useParams<PathParams>()
   return space && repoId ? `${space}/${repoId}/+` : ''

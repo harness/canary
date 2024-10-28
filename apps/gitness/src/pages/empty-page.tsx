f94b3d0224beb30e63da5af96de759ac329bde10
sanskar <c_sanskar.sehgal@harness.io>
2024-10-28 16:23:29 -0700

    adds empty pages for all routes

---
+++ apps/gitness/src/pages/empty-page.tsx
@@ -0,0 +1,23 @@
+import { NoData } from '@harnessio/playground'
+import { SandboxLayout } from '@harnessio/playground'
+import { useNavigate } from 'react-router-dom'
+
+export const EmptyPage = ({ pathName }: { pathName: string }) => {
+  const navigate = useNavigate()
+  //   console.log(navigate(-1))
+  return (
+    <SandboxLayout.Main>
+      <SandboxLayout.Content>
+        <NoData
+          iconName="no-search-magnifying-glass"
+          title={`${pathName} does not exist`}
+          description={[`Upgrade to harness-enterprise to access ${pathName}`]}
+          primaryButton={{
+            label: 'Take me back',
+            onClick: () => navigate(-1)
+          }}
+        />
+      </SandboxLayout.Content>
+    </SandboxLayout.Main>
+  )
+}

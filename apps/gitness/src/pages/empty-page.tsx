f85595609ee5847bab1129b80d5c37bd890d3d59
sanskar <c_sanskar.sehgal@harness.io>
2024-10-28 20:53:32 -0700

    centered empty pages

--- apps/gitness/src/pages/empty-page.tsx
+++ apps/gitness/src/pages/empty-page.tsx
@@ -1,22 +1,19 @@
 import { NoData } from '@harnessio/playground'
-import { SandboxLayout } from '@harnessio/playground'
 import { useNavigate } from 'react-router-dom'
 
 export const EmptyPage = ({ pathName }: { pathName: string }) => {
   const navigate = useNavigate()
   return (
-    <SandboxLayout.Main>
-      <SandboxLayout.Content>
-        <NoData
-          iconName="no-search-magnifying-glass"
-          title={`${pathName} does not exist`}
-          description={[`Upgrade to harness-enterprise to access ${pathName}`]}
-          primaryButton={{
-            label: 'Take me back',
-            onClick: () => navigate(-1)
-          }}
-        />
-      </SandboxLayout.Content>
-    </SandboxLayout.Main>
+    <div className="min-h-[100vh] flex items-center">
+      <NoData
+        iconName="no-search-magnifying-glass"
+        title={`${pathName} does not exist`}
+        description={[`Upgrade to harness-enterprise to access ${pathName}`]}
+        primaryButton={{
+          label: 'Take me back',
+          onClick: () => navigate(-1)
+        }}
+      />
+    </div>
   )
 }

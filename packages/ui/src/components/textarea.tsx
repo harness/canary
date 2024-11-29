a707800b3f4727616d5c78f75507b358dc5436af
Shaurya Kalia <shaurya.kalia@harness.io>
2024-11-29 22:11:52 +0530

    fix: textarea class

--- packages/ui/src/components/textarea.tsx
+++ packages/ui/src/components/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ..
   return (
     <textarea
       className={cn(
-        'border-borders-2 placeholder:text-foreground-5 flex min-h-[60px] w-full rounded border bg-transparent px-3 py-2 text-14 disabled:cursor-not-allowed disabled:opacity-50',
+        'border-borders-2 placeholder:text-foreground-4 flex min-h-[60px] w-full rounded border bg-transparent px-3 py-2 text-14 disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       ref={ref}

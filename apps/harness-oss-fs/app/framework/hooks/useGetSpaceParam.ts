0104d1e4cda6373dd519d2a3b38dcdcf48eb5267
Vivek <c_vivek.patel@harness.io>
2024-12-02 09:45:29 -0800

    Removed harness-oss and harness-oss-fs

--- apps/harness-oss-fs/app/framework/hooks/useGetSpaceParam.ts
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router'
-
-import { PathParams } from '~/RouteDefinitions'
-
-export function useGetSpaceURLParam(): string | undefined {
-  const { spaceId } = useParams<PathParams>()
-  return spaceId
-}
--- apps/harness-oss-fs/app/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare interface Window {
-  apiUrl?: string
-}

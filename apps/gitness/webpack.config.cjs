81dda52cf1f81488f380b6ddbd31e587f40eacf2
Pranesh <pranesh.g@harness.io>
2024-12-04 10:38:15 +0530

    Working button expose

---
+++ apps/gitness/webpack.config.cjs
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { container } = require('webpack')
+
+// import packageJson from './package.json' assert { type: 'json' }
+
+const { ModuleFederationPlugin } = container
+
+// const { dependencies } = packageJson
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: './src/dummy.ts',
+  devServer: {
+    port: 5137,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
+  // output: {
+  //   crossOriginLoading: 'anonymous'
+  // },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'gitness',
+      filename: 'remoteEntry.js',
+      exposes: {
+        // './PipelineList': './src/pages/pipeline/repo-pipeline-list.tsx',
+        './Button': './src/components/exportComponents/Button.tsx'
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: false,
+          eager: true
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: false,
+          eager: true
+        }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      crossorigin: 'anonymous'
+    })
+  ]
+}

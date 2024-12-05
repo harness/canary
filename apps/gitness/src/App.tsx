b7c3820e480fa94f1942a1dd5482881c56f2cc2b
Pranesh <pranesh.g@harness.io>
2024-12-05 21:52:54 +0530

    Nav link fix

--- apps/gitness/src/App.tsx
+++ apps/gitness/src/App.tsx
@@ -10,7 +10,8 @@ import {
   EmptyPage,
   ForgotPasswordPage as ForgotPasswordPageV2,
   NewPasswordPage as NewPasswordPageV2,
-  OTPPage as OTPPageV2
+  OTPPage as OTPPageV2,
+  SandboxLayout
 } from '@harnessio/ui/views'
 import {
   ForgotPasswordPage,
@@ -145,6 +146,38 @@ export default function App() {
       path: '/',
       element: <RootWrapper />,
       children: [
+        {
+          path: 'repos',
+          element: (
+            <SandboxLayout.Main hasLeftPanel hasHeader>
+              <h1>Repo</h1>
+            </SandboxLayout.Main>
+          )
+        },
+        {
+          path: 'pipelines',
+          element: (
+            <SandboxLayout.Main hasLeftPanel hasHeader>
+              <h1>pipelines</h1>
+            </SandboxLayout.Main>
+          )
+        },
+        {
+          path: 'executions',
+          element: (
+            <SandboxLayout.Main hasLeftPanel hasHeader>
+              <h1>executions</h1>
+            </SandboxLayout.Main>
+          )
+        },
+        {
+          path: 'databases',
+          element: (
+            <SandboxLayout.Main hasLeftPanel hasHeader>
+              <h1>databases</h1>
+            </SandboxLayout.Main>
+          )
+        },
         {
           path: ':spaceId/repos',
           element: <ReposListPage />
@@ -592,10 +625,10 @@ export default function App() {
       path: 'cloud-costs',
       element: <EmptyPage pathName="Cloud Cost Management" />
     },
-    {
-      path: 'databases',
-      element: <EmptyPage pathName="Databases" />
-    },
+    // {
+    //   path: 'databases',
+    //   element: <h1>DBs</h1>
+    // },
     {
       path: 'incidents',
       element: <EmptyPage pathName="Incidents" />
--- packages/ui/src/data/pinned-menu-items-data.ts
+++ packages/ui/src/data/pinned-menu-items-data.ts
@@ -2,25 +2,25 @@ import { NavbarItemType } from '@components/navbar/types'
 import { TFunction } from 'i18next'
 
 export const getPinnedMenuItemsData = (t: TFunction): NavbarItemType[] => [
-  {
-    id: 0,
-    iconName: 'repositories-gradient',
-    title: t('component:navbar.repositories'),
-    description: 'Integrated & familiar git experience.',
-    to: '/repos'
-  },
-  {
-    id: 1,
-    iconName: 'pipelines-gradient',
-    title: t('component:navbar.pipelines'),
-    description: 'Up to 4X faster than other solutions.',
-    to: '/pipelines'
-  },
-  {
-    id: 2,
-    iconName: 'execution-gradient',
-    title: t('component:navbar.executions'),
-    description: 'Optimize feature rollout velocity.',
-    to: '/executions'
-  }
+  // {
+  //   id: 0,
+  //   iconName: 'repositories-gradient',
+  //   title: t('component:navbar.repositories'),
+  //   description: 'Integrated & familiar git experience.',
+  //   to: '/repos'
+  // },
+  // {
+  //   id: 1,
+  //   iconName: 'pipelines-gradient',
+  //   title: t('component:navbar.pipelines'),
+  //   description: 'Up to 4X faster than other solutions.',
+  //   to: '/pipelines'
+  // },
+  // {
+  //   id: 2,
+  //   iconName: 'execution-gradient',
+  //   title: t('component:navbar.executions'),
+  //   description: 'Optimize feature rollout velocity.',
+  //   to: '/executions'
+  // }
 ]

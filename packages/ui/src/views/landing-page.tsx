517c04c770e5d54273cdf6be63709285d72759d6
sanskar <c_sanskar.sehgal@harness.io>
2024-12-07 20:03:36 -0800

    feat: add i18n

--- packages/ui/src/views/landing-page.tsx
+++ packages/ui/src/views/landing-page.tsx
@@ -9,7 +9,7 @@ import {
   Icon,
   Text
 } from '@/components'
-import { SandboxLayout } from '@/views'
+import { SandboxLayout, TranslationStore } from '@/views'
 
 interface TypesSpace {
   created?: number
@@ -25,22 +25,26 @@ interface TypesSpace {
 
 interface LandingPageProps {
   spaces: TypesSpace[]
+  useTranslationStore: () => TranslationStore
 }
 
-export const LandingPageView: React.FC<LandingPageProps> = ({ spaces }) => {
+export const LandingPageView: React.FC<LandingPageProps> = ({ spaces, useTranslationStore }) => {
   const navigate = useNavigate()
+  const { t } = useTranslationStore()
 
   return (
     <SandboxLayout.Main hasLeftPanel>
       <div className="flex h-full w-full flex-col place-content-center place-items-center gap-4 justify-center items-center">
         <div className="flex flex-col place-content-center place-items-center gap-2.5">
           <Text size={5} weight="medium">
-            Select a project to get started
+            {t('views:landingPage.selectProject', 'Select a project to get started')}
           </Text>
           <div className="flex flex-col">
             <Text size={2} weight="normal" align="center" color="tertiaryBackground">
-              Projects contain your repositories and pipelines. To start using Gitness, select a project or create a new
-              one.
+              {t(
+                'views:landingPage.description',
+                'Projects contain your repositories and pipelines. To start using Gitness, select a project or create a new one.'
+              )}
             </Text>
           </div>
           <div className="mt-4">
@@ -48,12 +52,12 @@ export const LandingPageView: React.FC<LandingPageProps> = ({ spaces }) => {
               <DropdownMenuTrigger asChild>
                 <Button variant="default" size={'lg'}>
                   <Text color="tertiary" className="mr-5">
-                    Select Project
+                    {t('views:landingPage.projectSelector', 'Select Project')}
                   </Text>
                   <Icon name="chevron-down" size={15} className="chevron-down" />
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="min-w-[185px]">
+              <DropdownMenuContent style={{ width: 'var(--radix-dropdown-menu-trigger-width)' }}>
                 {spaces?.length ? (
                   spaces.map(space => (
                     <DropdownMenuItem key={space.id} onClick={() => navigate(`${space.path}/repos`)}>
@@ -61,7 +65,9 @@ export const LandingPageView: React.FC<LandingPageProps> = ({ spaces }) => {
                     </DropdownMenuItem>
                   ))
                 ) : (
-                  <DropdownMenuItem disabled>No projects available</DropdownMenuItem>
+                  <DropdownMenuItem disabled>
+                    {t('views:landingPage.noProjects', 'No projects available')}
+                  </DropdownMenuItem>
                 )}
               </DropdownMenuContent>
             </DropdownMenu>

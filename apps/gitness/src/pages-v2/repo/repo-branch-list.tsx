71bff93cb416df46428fb3999f412102f94a807f
sanskar <c_sanskar.sehgal@harness.io>
2024-12-09 15:27:17 -0800

    feat: merge branch stores/ reduce API calls

--- apps/gitness/src/pages-v2/repo/repo-branch-list.tsx
+++ apps/gitness/src/pages-v2/repo/repo-branch-list.tsx
@@ -4,8 +4,6 @@ import { useParams } from 'react-router-dom'
 import { parseAsInteger, useQueryState } from 'nuqs'
 
 import {
-  //   ListBranchesQueryQueryParams,
-  // TypesBranchExtended,
   useCalculateCommitDivergenceMutation,
   useFindRepositoryQuery,
   useListBranchesQuery
@@ -17,38 +15,23 @@ import { useDebouncedQueryState } from '../../hooks/useDebouncedQueryState'
 import { useTranslationStore } from '../../i18n/stores/i18n-store'
 import { PathParams } from '../../RouteDefinitions'
 import { orderSortDate, PageResponseHeader } from '../../types'
-import { useRepoBranchStore } from './stores/repo-branch-store'
+import { useRepoBranchesStore } from './stores/repo-branches-store'
 
 // import CreateBranchDialog from './repo-branch-create'
-
-// const sortOptions = [
-//   { name: 'Date', value: 'date' },
-//   { name: 'Name', value: 'name' }
-// ]
 
 export function RepoBranchesListPage() {
   const repoRef = useGetRepoRef()
   const { spaceId, repoId } = useParams<PathParams>()
-  const { setBranchesData, setDefaultBranch, setBranchDivergence } = useRepoBranchStore()
+  const { setBranchesData, setDefaultBranch, setSpaceIdAndRepoId, setBranchDivergence } = useRepoBranchesStore()
 
-  //   const { sort } = useCommonFilter<ListBranchesQueryQueryParams['sort']>()
   const [query, _setQuery] = useDebouncedQueryState('query')
   const [page, setPage] = useQueryState('page', parseAsInteger.withDefault(1))
 
   //   const [isCreateBranchDialogOpen, setCreateBranchDialogOpen] = useState(false)
   //   const { data: { body: repoMetadata } = {} } = useFindRepositoryQuery({ repo_ref: repoRef })
-  const { data: { body: repoMetadata } = {} } = useFindRepositoryQuery(
-    {
-      repo_ref: repoRef
-    },
-    {
-      onSuccess: data => {
-        if (data.body) {
-          setDefaultBranch(data.body)
-        }
-      }
-    }
-  )
+  const { data: { body: repoMetadata } = {} } = useFindRepositoryQuery({
+    repo_ref: repoRef
+  })
 
   const { isLoading, data: { body: branches, headers } = {} } = useListBranchesQuery({
     queryParams: { page, query, order: orderSortDate.DESC, include_commit: true },
@@ -85,21 +68,24 @@ export function RepoBranchesListPage() {
     }
   }, [calculateBranchDivergence, branches, repoMetadata?.default_branch])
 
-  //   useEffect(() => {
-  //     if (branches) {
-  //       setBranchesData(branches, branchDivergence, repoMetadata?.default_branch)
-  //     }
-  //   }, [branches, branchDivergence, repoMetadata?.default_branch, setBranchesData])
+  useEffect(() => {
+    setSpaceIdAndRepoId(spaceId || '', repoId || '')
+  }, [spaceId, repoId, setSpaceIdAndRepoId])
+
+  useEffect(() => {
+    if (branches) {
+      setBranchesData(branches, branchDivergence, repoMetadata?.default_branch)
+    }
+  }, [branches, repoMetadata?.default_branch, setBranchesData])
+
+  useEffect(() => {
+    setDefaultBranch(repoMetadata || null)
+  }, [repoMetadata, setDefaultBranch])
 
   return (
     <RepoBranchListView
       isLoading={isLoading}
-      repoId={repoId}
-      spaceId={spaceId}
-      //   repoMetadata={repoMetadata}
-      //   branches={branches}
-      //   branchDivergence={branchDivergence}
-      useRepoBranchStore={useRepoBranchStore}
+      useRepoBranchesStore={useRepoBranchesStore}
       xNextPage={xNextPage}
       xPrevPage={xPrevPage}
       page={page}

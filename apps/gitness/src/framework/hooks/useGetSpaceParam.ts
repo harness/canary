281b53b8c324401426edfc9b31c367f9b81703b6
Vivek <c_vivek.patel@harness.io>
2024-12-10 15:15:59 -0800

    Removed unused code and fixed lint and type errors

--- apps/gitness/src/framework/hooks/useGetSpaceParam.ts
+++ apps/gitness/src/framework/hooks/useGetSpaceParam.ts
@@ -6,6 +6,8 @@ import { MFEContext } from '../context/MFEContext'
 
 export function useGetSpaceURLParam(): string | undefined {
   const { spaceId } = useParams<PathParams>()
-  const { scope: { accountId, orgIdentifier, projectIdentifier } = {} } = useContext(MFEContext)
+  const {
+    scope: { accountId, orgIdentifier, projectIdentifier }
+  } = useContext(MFEContext)
   return spaceId || `${accountId}/${orgIdentifier}/${projectIdentifier}`
 }

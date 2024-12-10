dd22d5abed477414dc62e68ee8e7aea24d05e828
Vivek <c_vivek.patel@harness.io>
2024-12-10 15:15:59 -0800

    Removed unused code and fixed lint and type errors

--- apps/gitness/src/framework/context/MFEContext.tsx
+++ apps/gitness/src/framework/context/MFEContext.tsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 import { Scope } from '@harness/microfrontends'
 
 interface IMFEContext {
-  scope?: Scope
+  scope: Scope
 }
 export const MFEContext = createContext<IMFEContext>({
   scope: {}

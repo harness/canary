91baa1a601bde618c690d59de12c4470348b0a3d
Vivek <c_vivek.patel@harness.io>
2024-12-10 14:58:02 -0800

    Architectural changes for MFE to make API calls succeed

--- apps/gitness/src/framework/hooks/useGetSpaceParam.ts
+++ apps/gitness/src/framework/hooks/useGetSpaceParam.ts
@@ -1,8 +1,11 @@
+import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 
 import type { PathParams } from '../../RouteDefinitions'
+import { MFEContext } from '../context/MFEContext'
 
 export function useGetSpaceURLParam(): string | undefined {
   const { spaceId } = useParams<PathParams>()
-  return spaceId
+  const { scope: { accountId, orgIdentifier, projectIdentifier } = {} } = useContext(MFEContext)
+  return spaceId || `${accountId}/${orgIdentifier}/${projectIdentifier}`
 }

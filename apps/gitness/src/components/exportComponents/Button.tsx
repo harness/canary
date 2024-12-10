2f10172c507042f0b0fc20d064a656a591ac3232
Vivek <c_vivek.patel@harness.io>
2024-12-10 15:30:07 -0800

    Removed unused files

--- apps/gitness/src/components/exportComponents/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button as BT } from '@harnessio/ui/components'
-
-import './globalStyle.css'
-
-export default function Button({ ...props }) {
-  console.log('renderUrl', props)
-
-  return <BT>test</BT>
-}

e7d3dda77f4a38625d6aeb214ade96345ffcd3e5
Ilya Topilskii <iatopilskii@gmail.com>
2024-12-11 09:33:18 +0100

    fix: small condition fix

--- packages/ui/src/views/repo/pull-request/components/pull-request-item-description.tsx
+++ packages/ui/src/views/repo/pull-request/components/pull-request-item-description.tsx
@@ -20,17 +20,17 @@ export const PullRequestItemDescription: FC<PullRequestItemDescriptionProps> = (
   timestamp
 }) => {
   return (
-    <div className="inline-flex max-w-full items-center gap-1.5 pl-[22px] text-14 leading-none text-foreground-4">
+    <div className="text-14 text-foreground-4 inline-flex max-w-full items-center gap-1.5 pl-[22px] leading-none">
       <p>
         {`#${number}`} opened {timestamp} by {author}
       </p>
 
-      <span className="pointer-events-none h-2.5 w-px bg-borders-2" aria-hidden />
+      <span className="bg-borders-2 pointer-events-none h-2.5 w-px" aria-hidden />
 
       <p>{reviewRequired ? 'Review required' : 'Draft'}</p>
 
       {/* TODO: where did tasks go? */}
-      {tasks && tasks > 0 && (
+      {!!tasks && tasks > 0 && (
         <div className="flex items-center gap-0.5">
           <Icon className="text-icons-1" size={12} name="tasks" />
           <p>

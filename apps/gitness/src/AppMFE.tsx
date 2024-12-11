29d0e19927b3b7f46dbec349908a55719e721d63
Vivek <c_vivek.patel@harness.io>
2024-12-10 18:10:18 -0800

    Added missing repo routes

--- apps/gitness/src/AppMFE.tsx
+++ apps/gitness/src/AppMFE.tsx
@@ -10,14 +10,25 @@ import { NuqsAdapter } from 'nuqs/adapters/react-router'
 import { TooltipProvider } from '@harnessio/canary'
 import { CodeServiceAPIClient } from '@harnessio/code-service-client'
 
+import Breadcrumbs from './components/breadcrumbs/breadcrumbs'
 import { AppProvider } from './framework/context/AppContext'
 import { ExitConfirmProvider } from './framework/context/ExitConfirmContext'
+import { ExplorerPathsProvider } from './framework/context/ExplorerPathsContext'
 import { MFEContext } from './framework/context/MFEContext'
 import { ThemeProvider } from './framework/context/ThemeContext'
 import { queryClient } from './framework/queryClient'
 import i18n from './i18n/i18n'
+import { CreatePullRequest } from './pages-v2/pull-request/pull-request-compare'
+import SandboxPullRequestListPage from './pages-v2/pull-request/pull-request-list'
+import { RepoBranchesListPage } from './pages-v2/repo/repo-branch-list'
+import { RepoCode } from './pages-v2/repo/repo-code'
+import RepoCommitsPage from './pages-v2/repo/repo-commits'
+import { CreateRepo } from './pages-v2/repo/repo-create-page'
 import ReposListPage from './pages-v2/repo/repo-list'
+import { RepoSidebar } from './pages-v2/repo/repo-sidebar'
 import RepoSummaryPage from './pages-v2/repo/repo-summary'
+import WebhookListPage from './pages-v2/webhooks/webhook-list'
+import { RepoImportContainer } from './pages/repo/repo-import-container'
 
 const BASE_URL_PREFIX = `${window.apiUrl || ''}/api/v1`
 
@@ -46,7 +57,30 @@ export default function AppMFE({ scope, renderUrl, on401 }: ChildComponentProps)
                     <BrowserRouter basename={`/ng${renderUrl}`}>
                       <Routes>
                         <Route path="repos" element={<ReposListPage />} />
+                        <Route path="repos/create" element={<CreateRepo />} />
+                        <Route path="repos/import" element={<RepoImportContainer />} />
+
                         <Route path="repos/:repoId" element={<RepoSummaryPage />} />
+                        <Route path="repos/:repoId/commits" element={<RepoCommitsPage />} />
+                        <Route path="repos/:repoId/branches" element={<RepoBranchesListPage />} />
+                        <Route path="repos/:repoId/webhooks" element={<WebhookListPage />} />
+
+                        <Route
+                          path="repos/:repoId/code"
+                          element={
+                            <ExplorerPathsProvider>
+                              <RepoSidebar />
+                            </ExplorerPathsProvider>
+                          }
+                        >
+                          <Route index element={<RepoCode />} />
+                          <Route path="*" element={<RepoCode />} />
+                        </Route>
+
+                        <Route path="repos/:repoId/pulls">
+                          <Route index element={<SandboxPullRequestListPage />} />
+                          <Route path="compare/:diffRefs*?" element={<CreatePullRequest />} />
+                        </Route>
                       </Routes>
                     </BrowserRouter>
                   </MFEContext.Provider>

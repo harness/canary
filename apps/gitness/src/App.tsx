9e3c0ecb2b812d8db4cf15e7e90538c87670896f
Calvin Lee <cjlee@ualberta.ca>
2024-12-12 02:02:16 -0700

    feat: [pipe-24076]: add pull request layout and skeleton for pr details page-checks
    Signed-off-by: Calvin Lee <cjlee@ualberta.ca>

--- apps/gitness/src/App.tsx
+++ apps/gitness/src/App.tsx
@@ -270,7 +270,7 @@ export default function App() {
                 },
                 {
                   path: 'commits',
-                  element: <PullRequestCommitsPage />
+                  element: <>commits</>
                 },
                 {
                   path: 'changes',

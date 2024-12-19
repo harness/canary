3958065da21f52df008ec3126353b8e3ecdd1c06
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-12-19 13:13:09 -0800

    fix: repo visibility top margin

--- packages/ui/src/views/repo/repo-settings/components/repo-settings-general-form.tsx
+++ packages/ui/src/views/repo/repo-settings/components/repo-settings-general-form.tsx
@@ -179,7 +179,7 @@ export const RepoSettingsGeneralForm: React.FC<{
             <Text className="leading-none text-foreground-2" size={2}>
               {t('views:repos.visibility', 'Visibility')}
             </Text>
-            <RadioGroup className="mt-4" value={accessValue} onValueChange={handleAccessChange} id="access">
+            <RadioGroup className="mt-6" value={accessValue} onValueChange={handleAccessChange} id="access">
               <Option
                 control={<RadioButton value="1" id="access-public" />}
                 id="access-public"

bf9c07f6411a1f96369335f1b0b96ba207cd5a8a
Kevin Nagurski <Kevin.nagurski@harness.io>
2024-12-19 20:34:19 +0000

    pick feat: [XD-47]: Initial effort for component library docs

--- apps/design-system/src/App.tsx
+++ apps/design-system/src/App.tsx
@@ -1,20 +1,18 @@
 import { FC } from 'react'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
-import { ExitConfirmProvider } from './context/exit-confirm-context.tsx'
-import ViewPreview from './pages/view-preview/view-preview.tsx'
+import ComponentPage from '@/pages/component-page.tsx'
+import ViewPreview from '@/pages/view-preview/view-preview.tsx'
+import DocsLayout from '@components/docs-layout/docs-layout.tsx'
 
 const router = createBrowserRouter([
   { path: '/view-preview/*', element: <ViewPreview /> },
-  { path: '/*', element: <Navigate to="/view-preview" /> } // temp redirect to view preview
+  { path: '/docs/*', element: <DocsLayout />, children: [{ path: 'components/*', element: <ComponentPage /> }] },
+  { path: '/*', element: <Navigate to="/docs" /> } // temp redirect to view preview
 ])
 
 const App: FC = () => {
-  return (
-    <ExitConfirmProvider>
-      <RouterProvider router={router} />
-    </ExitConfirmProvider>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- apps/design-system/src/pages/view-preview/repo-files-view-wrapper.tsx
+++ apps/design-system/src/pages/view-preview/repo-files-view-wrapper.tsx
@@ -1,10 +1,10 @@
 import { FC, PropsWithChildren, useCallback } from 'react'
 
+import { OpenapiContentInfo, repoFilesStore } from '@subjects/views/repo-files/components/repo-files-store.ts'
+import { noop, useTranslationsStore } from '@utils/viewUtils.ts'
+
 import { FileExplorer } from '@harnessio/ui/components'
 import { BranchSelectorTab, IBranchSelectorStore, RepoSidebar as RepoSidebarView } from '@harnessio/ui/views'
-
-import { noop, useTranslationsStore } from '../../utils.ts'
-import { OpenapiContentInfo, repoFilesStore } from '../../views/repo-files/components/repo-files-store.ts'
 
 const sortEntriesByType = (entries: OpenapiContentInfo[]): OpenapiContentInfo[] => {
   return entries.sort((a, b) => {

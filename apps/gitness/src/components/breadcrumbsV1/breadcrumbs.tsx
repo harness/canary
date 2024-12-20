31ac1c0d99aac6f8eaa9d61fd9464096ac764dad
Vardan Bansal <vardan.bansal@harness.io>
2024-12-20 15:59:21 +0530

    refactor Breadcrumbs to use packages/ui components

--- apps/gitness/src/components/breadcrumbsV1/breadcrumbs.tsx
+++ apps/gitness/src/components/breadcrumbsV1/breadcrumbs.tsx
@@ -1,37 +1,45 @@
-import { Link, useMatches } from 'react-router-dom'
+import { useMatches } from 'react-router-dom'
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator
+} from '@harnessio/ui/components'
 
 function Breadcrumbs() {
   const matches = useMatches()
 
   return (
-    <nav className="mb-4 flex items-start gap-1">
-      {/**
-       * Fixed/Default links
-       */}
-      <Link to="/" className="text-blue-500 hover:underline">
-        Home
-      </Link>
-      {matches.map((match, index) => {
-        /** @ts-expect-error should be of type "CustomHandle". @TODO fix this properly */
-        const { breadcrumb } = match.handle || {}
-        const isLast = index === matches.length - 1
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          {/**
+           * Fixed/Default links
+           */}
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {matches.map((match, index) => {
+          /** @ts-expect-error should be of type "CustomHandle". @TODO fix this properly */
+          const { breadcrumb } = match.handle || {}
+          const isLast = index === matches.length - 1
 
-        if (!breadcrumb) return null
+          if (!breadcrumb) return null
 
-        return (
-          <div key={match.pathname} className="flex">
-            <span className="ml-1 mr-1">/</span>
-            {isLast ? (
-              breadcrumb(match.params)
-            ) : (
-              <Link to={match.pathname} className="text-blue-500 hover:underline">
-                {breadcrumb(match.params)}
-              </Link>
-            )}
-          </div>
-        )
-      })}
-    </nav>
+          return (
+            <BreadcrumbItem>
+              <BreadcrumbSeparator>/</BreadcrumbSeparator>
+              {isLast ? (
+                breadcrumb(match.params)
+              ) : (
+                <BreadcrumbLink href={match.pathname}>{breadcrumb(match.params)}</BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+          )
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
   )
 }
 

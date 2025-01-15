361305b5e67e9635eff485203cbfdebbfb11db3b
Vardan Bansal <vardan.bansal@harness.io>
2025-01-14 16:09:05 -0800

    Cleanup unused code

--- packages/ui/src/components/navbar/data.ts
+++ packages/ui/src/components/navbar/data.ts
@@ -8,35 +8,3 @@ export const getAdminMenuItem = (t: TFunction): Pick<NavbarItemType, 'title' | '
     iconName: 'settings-1'
   }
 }
-
-export const getUserMenuItems = (t: TFunction): UserMenuItemType[] => [
-  {
-    key: UserMenuKeys.ACCOUNT,
-    iconName: 'user',
-    title: t('component:navbar.profile', 'Profile'),
-    to: '/profile',
-    isSeparated: false
-  },
-  {
-    key: UserMenuKeys.THEME,
-    iconName: 'paint',
-    title: t('component:navbar.theme', 'Theme'),
-    to: '/theme',
-    isSeparated: false
-  },
-  {
-    key: UserMenuKeys.CUSTOM_NAV,
-    iconName: 'navigation',
-    title: t('component:navbar.customNav', 'Customize navigation'),
-    to: null,
-    isSeparated: false
-  },
-
-  {
-    key: UserMenuKeys.LOG_OUT,
-    iconName: 'logOut',
-    title: t('component:navbar.logout', 'Log out'),
-    to: null,
-    isSeparated: true
-  }
-]

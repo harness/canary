a9e2170f0946bcd273fa4d7ca3ad99870479b224
Kevin Nagurski <Kevin.nagurski@harness.io>
2025-01-16 10:23:24 +0000

    initial ui lib integration

--- apps/portal/tailwind.config.mjs
+++ apps/portal/tailwind.config.mjs
@@ -1,18 +1,12 @@
-import colors from 'tailwindcss/colors';
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from "@astrojs/starlight-tailwind";
+import tailwindConfig from "@harnessio/ui/tailwind.config";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  presets: [tailwindConfig],
+  content: [
+    "node_modules/@harnessio/ui/src/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  plugins: [starlightPlugin(), require("tailwindcss-animate")],
 };

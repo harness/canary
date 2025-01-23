6e265eff7930b131d29afa73848a7c1c6e46d2fb
sanskar <c_sanskar.sehgal@harness.io>
2025-01-23 13:53:31 -0800

    chore: hide 3-dot-menu

--- packages/ui/src/views/repo/pull-request/compare/components/pull-request-compare-diff-list.tsx
+++ packages/ui/src/views/repo/pull-request/compare/components/pull-request-compare-diff-list.tsx
@@ -31,11 +31,11 @@ const LineTitle: FC<LineTitleProps> = ({ text }) => (
       <p className="font-medium">{text}</p>
       <CopyButton name={text} />
     </div>
-    <div className="inline-flex items-center gap-x-6">
+    {/* <div className="inline-flex items-center gap-x-6">
       <div className="flex items-center gap-2">
         <Icon name="ellipsis" size={12} />
       </div>
-    </div>
+    </div> */}
   </div>
 )
 

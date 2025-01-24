d879c11a19475cbfbe368dde04265714ef57fb09
harish-viswa <harish.viswanathan@harness.io>
2025-01-24 15:28:16 +0530

    fix: updated filters rendering according to order

--- packages/filters/src/FiltersContent.tsx
+++ packages/filters/src/FiltersContent.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, useRef } from 'react'
+
 import { useFiltersContext } from './Filters'
 import { FilterStatus, InitializeFiltersConfigType } from './types'
 
@@ -11,35 +12,45 @@ const FiltersContent: React.FC<FiltersContentProps> = ({ children, className }) 
   const { visibleFilters, addInitialFilters } = useFiltersContext()
   const initializedFiltersRef = useRef(false)
 
-  const { components, filtersConfig } = React.Children.toArray(children).reduce<{
-    components: ReactNode[]
-    filtersConfig: Record<string, InitializeFiltersConfigType>
-  }>(
-    (acc, child) => {
-      if (React.isValidElement(child) && child.props.filterKey !== null && typeof child.props.filterKey === 'string') {
-        if (visibleFilters.includes(child.props.filterKey)) {
-          acc.components.push(child)
-        }
-
-        acc.filtersConfig[child.props.filterKey] = {
-          parser: child.props.parser,
-          isSticky: child.props.sticky,
-          state: FilterStatus.HIDDEN
-        }
-      } else {
-        acc.components.push(child)
-      }
-      return acc
+  const reducerInitialState = {
+    components: {
+      filters: {},
+      nonFilters: []
     },
-    { components: [], filtersConfig: {} }
-  )
+    filtersConfig: {}
+  }
+
+  const { components, filtersConfig } = React.Children.toArray(children).reduce<{
+    components: {
+      filters: Record<string, ReactNode>
+      nonFilters: ReactNode[]
+    }
+    filtersConfig: Record<string, InitializeFiltersConfigType>
+  }>((acc, child) => {
+    if (React.isValidElement(child) && child.props.filterKey !== null && typeof child.props.filterKey === 'string') {
+      if (visibleFilters.includes(child.props.filterKey)) {
+        acc.components.filters[child.props.filterKey] = child
+      }
+
+      acc.filtersConfig[child.props.filterKey] = {
+        parser: child.props.parser,
+        isSticky: child.props.sticky,
+        state: FilterStatus.HIDDEN
+      }
+    } else {
+      acc.components.nonFilters.push(child)
+    }
+    return acc
+  }, reducerInitialState)
 
   if (initializedFiltersRef.current === false) {
     addInitialFilters(filtersConfig)
     initializedFiltersRef.current = true
   }
 
-  return <div className={className}>{components}</div>
+  const renderableComponents = visibleFilters.map(filterKey => components.filters[filterKey])
+
+  return <div className={className}>{renderableComponents.concat(components.nonFilters)}</div>
 }
 
 export default FiltersContent

4da5f57f325acde6f1d52e428f3560f4c1ba004b
Vardan Bansal <vardan.bansal@harness.io>
2025-01-27 15:23:14 -0800

    Cleanup

--- apps/gitness/src/framework/hooks/useDocumentTitle.ts
+++ apps/gitness/src/framework/hooks/useDocumentTitle.ts
@@ -1,14 +1,19 @@
 import { useEffect } from 'react'
 import { useMatches } from 'react-router-dom'
 
+import { useTranslationStore } from '../../i18n/stores/i18n-store'
 import { CustomHandle } from '../routing/types'
 
 const useDocumentTitle = () => {
+  const { t } = useTranslationStore()
   const matches = useMatches()
 
   useEffect(() => {
-    const handle = matches[matches.length - 1]?.handle as CustomHandle | undefined // Find the current route's handle
-    document.title = handle?.documentTitle?.(matches[matches.length - 1]?.params) ?? 'Harness Open Source'
+    const currentMatch = matches.at(-1) // Get the last match
+    if (currentMatch) {
+      const { documentTitle } = (currentMatch?.handle || {}) as CustomHandle
+      document.title = documentTitle?.(currentMatch.params) ?? t('views:app.harnessOpenSource', 'Harness Open Source')
+    }
   }, [matches])
 }
 

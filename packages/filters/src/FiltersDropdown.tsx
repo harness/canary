4242000e0c75a9dc4f3bb7c24ce27909df69493c
Harish V <164648608+harish-viswa@users.noreply.github.com>
2025-01-27 15:31:43 +0530

    Enhancements on Filters (#837)
    * fix: updated filters rendering according to order
    
    * feat: added support for defaultValues to filters
    
    * feat: added default value support
    
    * feat: added support for default value on-reset
    
    * fix: addressed review comments
    
    * feat: add onchange for filters

--- packages/filters/src/FiltersDropdown.tsx
+++ packages/filters/src/FiltersDropdown.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from 'react'
+
 import { useFiltersContext } from './Filters'
 
 export interface FiltersDropdownProps<T, K extends keyof T> {
-  children: (addFilter: (filterKey: K) => void, availableFilters: K[]) => ReactNode
+  children: (addFilter: (filterKey: K) => void, availableFilters: K[], resetFilters: () => void) => ReactNode
 }
 
 const FiltersDropdown = <T, K extends keyof T>({ children }: FiltersDropdownProps<T, K>): React.ReactElement | null => {
-  const { addFilter: addFilterContext, availableFilters } = useFiltersContext<any>()
+  const { addFilter: addFilterContext, availableFilters, resetFilters } = useFiltersContext<any>()
 
   const addFilter = (filterKey: K) => {
     addFilterContext(filterKey)
@@ -16,7 +17,7 @@ const FiltersDropdown = <T, K extends keyof T>({ children }: FiltersDropdownProp
     return availableFilters as K[]
   }
 
-  return <div id="filters-dropdown">{children(addFilter, getAvailableFilters())}</div>
+  return <div id="filters-dropdown">{children(addFilter, getAvailableFilters(), resetFilters)}</div>
 }
 
 export default FiltersDropdown

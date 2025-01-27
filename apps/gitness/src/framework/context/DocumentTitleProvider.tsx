7559bc8f6fdadfe755edfcd921729faf279131d0
Vardan Bansal <vardan.bansal@harness.io>
2025-01-27 14:42:56 -0800

    fix lint check

--- apps/gitness/src/framework/context/DocumentTitleProvider.tsx
+++ apps/gitness/src/framework/context/DocumentTitleProvider.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import useDocumentTitle from '../hooks/useDocumentTitle'
 
 export const DocumentTitleProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {

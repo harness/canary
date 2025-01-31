90016ae34fbc055bb771cbfa560b60295566e885
Srdjan Arsic <srdjan.arsic@harness.io>
2025-01-31 20:13:32 +0100

    format pipeline yaml

--- apps/design-system/src/subjects/views/pipeline-edit/mocks/pipelineYaml1.ts
+++ apps/design-system/src/subjects/views/pipeline-edit/mocks/pipelineYaml1.ts
@@ -2,24 +2,23 @@ export const pipelineYaml1 = `pipeline:
   stages:
     - group:
         stages:
-            - parallel:
-                stages:
-                    - steps:
-                        - run: go build
-                        - run: go test
-                    - steps:
-                        - run: npm test
-            - group:
-                stages:
-                    - steps:
-                        - group:
-                            steps:
-                            - run: go build
-                            - run: go test
+          - parallel:
+              stages:
+                - steps:
+                    - run: go build
+                    - run: go test
+                - steps:
+                    - run: npm test
+          - group:
+              stages:
+                - steps:
+                    - group:
+                        steps:
+                          - run: go build
+                          - run: go test
 
-                        - parallel:
-                            steps:
-                                - run: go build
-                                - run: go test
-                     
+                    - parallel:
+                        steps:
+                          - run: go build
+                          - run: go test      
 `

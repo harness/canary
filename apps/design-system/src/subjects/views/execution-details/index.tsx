f8b68af37037b74f1ba1568425dad896dfe41aa8
Vardan Bansal <vardan.bansal@harness.io>
2025-02-03 15:54:30 -0800

    fix layout with grid

--- apps/design-system/src/subjects/views/execution-details/index.tsx
+++ apps/design-system/src/subjects/views/execution-details/index.tsx
@@ -6,7 +6,7 @@ export const ExecutionDetailsView = () => {
   return (
     <div className="flex flex-col">
       <ExecutionHeader commitName="8fbru3ix" branchName="master" title="#311. Alerting docs: adds sns integration" />
-      <div className="flex flex-1">
+      <div className="grid p-4" style={{ gridTemplateColumns: '1fr 3fr' }}>
         <ExecutionTree
           defaultSelectedId="initialize"
           elements={elements}

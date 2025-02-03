6825c37fac541c6ac9ea9b4f8556a2a10d55bdfd
Vardan Bansal <vardan.bansal@harness.io>
2025-02-03 13:46:08 -0800

    Cleanup view wrapper

---
+++ apps/design-system/src/subjects/views/execution-details/index.tsx
@@ -0,0 +1,26 @@
+import { ExecutionTree, StageExecution } from '@harnessio/ui/views'
+
+import { elements, logs, stages } from './mocks/mock-data'
+
+export const ExecutionDetailsView = () => {
+  return (
+    <div className="flex flex-1">
+      <ExecutionTree
+        defaultSelectedId="initialize"
+        elements={elements}
+        onSelectNode={({ parentId, childId }: { parentId: string; childId: string }) => {
+          console.log(`Selected node: Parent ${parentId}, Child ${childId}`)
+        }}
+      />
+      <StageExecution
+        logs={logs}
+        onCopy={() => {}}
+        onDownload={() => {}}
+        onEdit={() => {}}
+        onStepNav={() => {}}
+        selectedStepIdx={0}
+        stage={stages[0]}
+      />
+    </div>
+  )
+}

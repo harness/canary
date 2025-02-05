329d4561edd80cfb18afe1e8ef91d27c69082815
Abhinav Rastogi <abhinav.rastogi@harness.io>
2025-02-05 15:07:52 -0800

    fix padding

--- packages/ui/src/components/tabnav.tsx
+++ packages/ui/src/components/tabnav.tsx
@@ -2,7 +2,7 @@ import { NavLink, NavLinkProps } from 'react-router-dom'
 
 const TabNavRoot: React.FC = ({ children }) => {
   return (
-    <div className="inline-flex h-[44px] w-full items-center justify-start gap-6 border-b border-border-background px-8 text-muted-foreground">
+    <div className="inline-flex h-[44px] w-full items-center justify-start gap-6 border-b border-border-background px-6 text-muted-foreground">
       {children}
     </div>
   )
@@ -13,8 +13,8 @@ const commonClasses = 'flex h-full items-center text-center cursor-pointer'
 const TabNavItem: React.FC<NavLinkProps> = ({ children, ...props }) => {
   return (
     <NavLink
-      {...props}
       className={({ isActive }) => (isActive ? `${commonClasses} text-primary border-b border-primary` : commonClasses)}
+      {...props}
     >
       {children}
     </NavLink>

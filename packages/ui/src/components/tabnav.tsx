dc26178bb9b4640f8a7b3a6aae5fa7e935481488
Abhinav Rastogi <abhinav.rastogi@harness.io>
2025-02-05 15:38:55 -0800

    update semantic html

--- packages/ui/src/components/tabnav.tsx
+++ packages/ui/src/components/tabnav.tsx
@@ -2,9 +2,9 @@ import { NavLink, NavLinkProps } from 'react-router-dom'
 
 const TabNavRoot: React.FC = ({ children }) => {
   return (
-    <div className="inline-flex h-[44px] w-full items-center justify-start gap-6 border-b border-border-background px-6 text-muted-foreground">
+    <nav className="inline-flex h-[44px] w-full items-center justify-start gap-6 border-b border-border-background px-6 text-muted-foreground">
       {children}
-    </div>
+    </nav>
   )
 }
 
@@ -13,6 +13,7 @@ const commonClasses = 'flex h-full items-center text-center cursor-pointer'
 const TabNavItem: React.FC<NavLinkProps> = ({ children, ...props }) => {
   return (
     <NavLink
+      role="tab"
       className={({ isActive }) => (isActive ? `${commonClasses} text-primary border-b border-primary` : commonClasses)}
       {...props}
     >

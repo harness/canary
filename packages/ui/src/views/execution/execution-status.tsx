bced66e0e0dd86fbdccc5c2ea6613565c3193443
Vivek <c_vivek.patel@harness.io>
2025-02-06 22:51:03 +0530

    Changed all text-success tokens to text-foreground-success

--- packages/ui/src/views/execution/execution-status.tsx
+++ packages/ui/src/views/execution/execution-status.tsx
@@ -59,15 +59,15 @@ const Badge: React.FC<ExecutionStatusProps & BadgeProps> = props => {
       return minimal ? (
         <div className="flex items-center gap-1">
           <div className="size-2 rounded-full bg-success" />
-          <span className="text-success">Success</span>
+          <span className="text-foreground-success">Success</span>
         </div>
       ) : (
         <div className="flex items-center gap-1 rounded-md">
-          <div className="flex items-center gap-0.5">
-            <CanaryIcon size={12} name="success" className="text-foreground-success" />
-            <span className="text-success">Success</span>
+          <div className="flex items-center gap-0.5 text-foreground-success">
+            <CanaryIcon size={12} name="success" />
+            <span>Success</span>
           </div>
-          {duration && <span className="text-success">{duration}</span>}
+          {duration && <span className="text-foreground-success">{duration}</span>}
         </div>
       )
     case ExecutionState.SKIPPED:

72a5b8a0d47f1219b1d0e5a5e143a961aeb5c5de
Vardan Bansal <vardan.bansal@harness.io>
2025-02-07 15:11:06 -0800

    handle animation by node type

--- apps/design-system/src/subjects/views/pipeline-edit/hooks/useAnimatePipeline.tsx
+++ apps/design-system/src/subjects/views/pipeline-edit/hooks/useAnimatePipeline.tsx
@@ -13,29 +13,75 @@ export const useAnimatePipeline = ({
 }): { nodes: AnyContainerNodeType[] } => {
   const [animatedNodes, setAnimatedNodes] = useState<AnyContainerNodeType[]>(nodes)
 
-  useEffect(() => {
-    if (!animatedNodes.length) return
+  const updateNodeState = (node: AnyContainerNodeType, state: PipelineNodeStatus) => {
+    node.data = { ...(node.data ?? {}), state }
+  }
 
-    let index = 0
-
-    const processNext = () => {
-      if (index >= animatedNodes.length) return
-
-      // Ensure that 'data' is an object and exists before spreading
-      const updatedNodes = [...animatedNodes]
-      updatedNodes[index].data = { ...(updatedNodes[index].data ?? {}), state: PipelineNodeStatus.Executing }
-
-      setAnimatedNodes(updatedNodes)
-
-      setTimeout(() => {
-        updatedNodes[index].data = { ...(updatedNodes[index].data ?? {}), state: PipelineNodeStatus.Success }
-        setAnimatedNodes([...updatedNodes])
-        index++
-        processNext() // Move to next node
-      }, delay * 1000)
+  const processParallelGroup = (node: AnyContainerNodeType, callback: () => void) => {
+    if (!node.children || node.children.length === 0) {
+      callback()
+      return
     }
 
-    processNext() // Start execution
+    let completedCount = 0
+    node.children.forEach(child => {
+      processStageOrStep(child, () => {
+        completedCount++
+        if (completedCount === node.children!.length) {
+          updateNodeState(node, PipelineNodeStatus.Success)
+          setAnimatedNodes([...animatedNodes])
+          callback()
+        }
+      })
+    })
+  }
+
+  const processSerialGroup = (node: AnyContainerNodeType, callback: () => void) => {
+    if (!node.children || node.children.length === 0) {
+      callback()
+      return
+    }
+
+    const processNext = (index: number) => {
+      if (index >= node.children!.length) {
+        updateNodeState(node, PipelineNodeStatus.Success)
+        setAnimatedNodes([...animatedNodes])
+        callback()
+        return
+      }
+
+      processStageOrStep(node.children![index], () => processNext(index + 1))
+    }
+
+    processNext(0)
+  }
+
+  const processStageOrStep = (node: AnyContainerNodeType, callback: () => void) => {
+    updateNodeState(node, PipelineNodeStatus.Executing)
+    setAnimatedNodes([...animatedNodes])
+
+    setTimeout(() => {
+      if (node.type === 'ParallelStageGroup') {
+        processParallelGroup(node, callback)
+      } else if (node.children && node.children.length > 0) {
+        processSerialGroup(node, callback)
+      } else {
+        updateNodeState(node, PipelineNodeStatus.Success)
+        setAnimatedNodes([...animatedNodes])
+        callback()
+      }
+    }, delay * 1000)
+  }
+
+  useEffect(() => {
+    if (animatedNodes.length === 0) return
+
+    const processNext = (index: number) => {
+      if (index >= animatedNodes.length) return
+      processStageOrStep(animatedNodes[index], () => processNext(index + 1))
+    }
+
+    processNext(0)
   }, [nodes, delay])
 
   return { nodes: animatedNodes }

323ac396a933d0f0410dbeb2a3dcaf0997c83072
Abhinav Rastogi <abhinav.rastogi@harness.io>
2025-02-07 12:02:02 -0800

    feat: design systems graph refactoring (#1020)
    * feat: design systems graph refactoring
    
    * feat: design systems graph refactoring
    
    * feat: design systems graph refactoring
    
    * Update shared-style-variables.css
    
    upd for pipeline cards colors
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css (#1021)
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * Update shared-style-variables.css
    
    * fix: minor changes
    
    ---------
    
    Co-authored-by: Anastasia Ramina <131158394+cxramina@users.noreply.github.com>
    Co-authored-by: Andrew Golovanov <spacewebdeveloper@gmail.com>

--- apps/design-system/src/subjects/views/pipeline-edit/pipeline-nodes/stage-node.tsx
+++ apps/design-system/src/subjects/views/pipeline-edit/pipeline-nodes/stage-node.tsx
@@ -1,3 +1,7 @@
+import { NodeMenuTrigger } from '@subjects/views/pipeline-edit/pipeline-nodes/components/node-menu-trigger.tsx'
+import { NodeTitle } from '@subjects/views/pipeline-edit/pipeline-nodes/components/node-title.tsx'
+import { getNestedStepsCount } from '@subjects/views/pipeline-edit/utils/common-step-utils'
+
 import { SerialNodeInternalType } from '@harnessio/pipeline-graph'
 import { Button, Icon } from '@harnessio/ui/components'
 import { cn } from '@harnessio/ui/views'
@@ -39,6 +43,8 @@ export function StageNode(props: StageNodeProps) {
   } = props
 
   const nodeData = node.data
+  const counter = getNestedStepsCount(node.children)
+
   return (
     <>
       <ExecutionStatus nodeData={nodeData} />
@@ -50,29 +56,9 @@ export function StageNode(props: StageNodeProps) {
         })}
       />
 
-      <div className="absolute inset-x-0 top-0 h-0">
-        <div
-          role="button"
-          tabIndex={0}
-          title={name}
-          className="text-primary-muted h-10 cursor-pointer truncate px-9 pt-2.5"
-          onClick={onHeaderClick}
-        >
-          {name}
-        </div>
-      </div>
+      <NodeTitle name={name} onHeaderClick={onHeaderClick} counter={counter} />
 
-      {onEllipsisClick && (
-        <Button
-          className="absolute right-2 top-2 z-10"
-          variant="ghost"
-          size="sm_icon"
-          onMouseDown={e => e.stopPropagation()}
-          onClick={onEllipsisClick}
-        >
-          <Icon className="text-icons-2" name="more-dots-fill" size={12} />
-        </Button>
-      )}
+      <NodeMenuTrigger onEllipsisClick={onEllipsisClick} />
 
       {!collapsed && isEmpty && (
         <Button
@@ -93,6 +79,7 @@ export function StageNode(props: StageNodeProps) {
           onClick={e => {
             onAddClick?.('before', e)
           }}
+          collapsed={collapsed}
         />
       )}
       <FloatingAddButton
@@ -101,6 +88,7 @@ export function StageNode(props: StageNodeProps) {
         onClick={e => {
           onAddClick?.('after', e)
         }}
+        collapsed={collapsed}
       />
 
       {collapsed ? <CollapsedGroupNode node={node} containerNodeType={'serial'} /> : children}

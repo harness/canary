81e39e95aa16356ebe079aa532afcda858d79461
Ilya Topilskii <iatopilskii@gmail.com>
2025-02-08 14:40:59 +0100

    feat: update execution-logs design (#1040)
    * feat: update execution-logs design
    
    * fix: fixes after rebasing
    
    * fix: fixes after merge and update logs drawer

--- packages/ui/src/views/execution/pipeline-status.tsx
+++ packages/ui/src/views/execution/pipeline-status.tsx
@@ -1,42 +1,68 @@
+import { FC } from 'react'
+
 import { ExecutionState } from '@views/repo/pull-request'
 
 import { ExecutionStatus } from './execution-status'
 
-const PipelineStatus = ({
+interface PipelineStatusProps {
+  status: ExecutionState
+  buildTime: string
+  createdTime?: string
+  commit?: string
+  branch?: string
+  startedTime?: string
+  delegateType?: string
+}
+
+const PipelineStatus: FC<PipelineStatusProps> = ({
   status,
   buildTime,
   createdTime,
   commit,
-  branch
-}: {
-  status: ExecutionState
-  buildTime: string
-  createdTime: string
-  commit: string
-  branch: string
+  branch,
+  startedTime,
+  delegateType
 }) => {
   return (
-    <div className="flex justify-between gap-12">
-      <div className="flex flex-col">
-        <span className="text-foreground-5">Commit</span>
-        <span className="text-primary">{commit}</span>
-      </div>
-      <div className="flex flex-col">
-        <span className="text-foreground-5">Branch</span>
-        <span className="text-primary">{branch}</span>
-      </div>
-      <div className="flex flex-col">
-        <span className="text-foreground-5">Status</span>
+    <div className="flex justify-between gap-11">
+      {commit && (
+        <div className="flex flex-col gap-1.5">
+          <span className="leading-tight text-foreground-4">Commit</span>
+          <span className="text-foreground-1">{commit}</span>
+        </div>
+      )}
+      {branch && (
+        <div className="flex flex-col gap-1.5">
+          <span className="leading-tight text-foreground-4">Branch</span>
+          <span className="text-foreground-1">{branch}</span>
+        </div>
+      )}
+      <div className="flex flex-col gap-1.5">
+        <span className="leading-tight text-foreground-4">Status</span>
         <ExecutionStatus.Badge status={status} minimal />
       </div>
-      <div className="flex flex-col">
-        <span className="text-foreground-5">Build time</span>
-        <span className="text-primary">{buildTime}</span>
+      <div className="flex flex-col gap-1.5">
+        <span className="leading-tight text-foreground-4">Build time</span>
+        <span className="text-tag-foreground-amber-1">{buildTime}</span>
       </div>
-      <div className="flex flex-col">
-        <span className="text-foreground-5">Created</span>
-        <span className="text-primary">{createdTime}</span>
-      </div>
+      {createdTime && (
+        <div className="flex flex-col gap-1.5">
+          <span className="leading-tight text-foreground-4">Created</span>
+          <span className="text-foreground-1">{createdTime}</span>
+        </div>
+      )}
+      {startedTime && (
+        <div className="flex flex-col gap-1.5">
+          <span className="leading-tight text-foreground-4">Started</span>
+          <span className="text-foreground-1">{startedTime}</span>
+        </div>
+      )}
+      {delegateType && (
+        <div className="flex flex-col gap-1.5">
+          <span className="leading-tight text-foreground-4">Delegate type</span>
+          <span className="text-foreground-1">{delegateType}</span>
+        </div>
+      )}
     </div>
   )
 }

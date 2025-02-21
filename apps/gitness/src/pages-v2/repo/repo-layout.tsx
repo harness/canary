63fce7d12dfd5ffe26db609a2bbbdce149bc77f0
sanskar <c_sanskar.sehgal@harness.io>
2025-02-21 12:06:40 -0800

    fix: tsc

--- apps/gitness/src/pages-v2/repo/repo-layout.tsx
+++ apps/gitness/src/pages-v2/repo/repo-layout.tsx
@@ -3,20 +3,16 @@ import { Outlet } from 'react-router-dom'
 import { RepoSubheader } from '@harnessio/ui/components'
 
 import { useIsMFE } from '../../framework/hooks/useIsMFE'
-// import { useRepoImportEvents } from '../../framework/hooks/useRepoImportEvent'
 import { useTranslationStore } from '../../i18n/stores/i18n-store'
 
 const RepoLayout = () => {
   const isMFE = useIsMFE()
-
-  // useRepoImportEvents()
 
   return (
     <>
       <div className="layer-high sticky top-[55px] bg-background-1">
         <RepoSubheader showPipelinesTab={!isMFE} useTranslationStore={useTranslationStore} />
       </div>
-      {/* <Toaster /> */}
       <Outlet />
     </>
   )

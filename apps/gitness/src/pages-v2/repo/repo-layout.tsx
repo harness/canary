376aa468f057bd6f8be373ff862a4a1d36f36180
sanskar <c_sanskar.sehgal@harness.io>
2025-02-21 11:59:38 -0800

    feat: create custom hook, rendered toaster in appshell

--- apps/gitness/src/pages-v2/repo/repo-layout.tsx
+++ apps/gitness/src/pages-v2/repo/repo-layout.tsx
@@ -1,69 +1,22 @@
-import { useCallback, useMemo } from 'react'
-import { Link, Outlet, useParams } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
-import { RepoRepositoryOutput } from '@harnessio/code-service-client'
-import { RepoSubheader, Toast, ToastAction, useToast } from '@harnessio/ui/components'
+import { RepoSubheader } from '@harnessio/ui/components'
 
-import { Toaster } from '../../components-v2/toaster'
-import { useRoutes } from '../../framework/context/NavigationContext'
-import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
 import { useIsMFE } from '../../framework/hooks/useIsMFE'
-import useSpaceSSE from '../../framework/hooks/useSpaceSSE'
+// import { useRepoImportEvents } from '../../framework/hooks/useRepoImportEvent'
 import { useTranslationStore } from '../../i18n/stores/i18n-store'
-import { PathParams } from '../../RouteDefinitions'
-import { SSEEvent } from '../../types'
-import { useRepoStore } from './stores/repo-list-store'
-import { transformRepoList } from './transform-utils/repo-list-transform'
 
 const RepoLayout = () => {
   const isMFE = useIsMFE()
-  const { update } = useToast()
-  const routes = useRoutes()
-  const { importToastId, importRepoIdentifier, setImportRepoIdentifier, setImportToastId, updateRepository } =
-    useRepoStore()
-  const { spaceId } = useParams<PathParams>()
-  const spaceURL = useGetSpaceURLParam() ?? ''
 
-  const onEvent = useCallback(
-    (eventData: RepoRepositoryOutput) => {
-      update({
-        id: importToastId ?? '',
-        title: 'Repository imported',
-        description: importRepoIdentifier,
-        duration: 5000,
-        action: (
-          <Link to={routes.toRepoSummary({ spaceId, repoId: importRepoIdentifier ?? '' })}>
-            <ToastAction altText="View repository">View</ToastAction>
-          </Link>
-        ),
-        variant: 'success'
-      })
-      const transformedRepo = transformRepoList([eventData])
-      updateRepository(transformedRepo[0])
-      // addRepository(transformedRepo[0])
-      setImportRepoIdentifier(null)
-      setImportToastId(null)
-    },
-    [importToastId]
-  )
-
-  const events = useMemo(() => [SSEEvent.REPO_IMPORTED], [])
-
-  useSpaceSSE({
-    space: spaceURL,
-    events,
-    onEvent,
-    shouldRun: true
-  })
+  // useRepoImportEvents()
 
   return (
     <>
       <div className="layer-high sticky top-[55px] bg-background-1">
         <RepoSubheader showPipelinesTab={!isMFE} useTranslationStore={useTranslationStore} />
       </div>
-      {/* <div className="absolute bottom-0 right-0"> */}
-      <Toaster />
-      {/* </div> */}
+      {/* <Toaster /> */}
       <Outlet />
     </>
   )
--- apps/gitness/src/pages-v2/repo/repo-list.tsx
+++ apps/gitness/src/pages-v2/repo/repo-list.tsx
@@ -1,19 +1,17 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-import { RepoRepositoryOutput, useDeleteRepositoryMutation, useListReposQuery } from '@harnessio/code-service-client'
+import { useDeleteRepositoryMutation, useListReposQuery } from '@harnessio/code-service-client'
 import { ToastAction, useToast } from '@harnessio/ui/components'
 import { RepositoryType, SandboxRepoListPage } from '@harnessio/ui/views'
 
-import { Toaster } from '../../components-v2/toaster'
 import { useRoutes } from '../../framework/context/NavigationContext'
 import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
 import { useQueryState } from '../../framework/hooks/useQueryState'
-import useSpaceSSE from '../../framework/hooks/useSpaceSSE'
 import usePaginationQueryStateWithStore from '../../hooks/use-pagination-query-state-with-store'
 import { useTranslationStore } from '../../i18n/stores/i18n-store'
 import { PathParams } from '../../RouteDefinitions'
-import { PageResponseHeader, SSEEvent } from '../../types'
+import { PageResponseHeader } from '../../types'
 import { useRepoStore } from './stores/repo-list-store'
 import { transformRepoList } from './transform-utils/repo-list-transform'
 
@@ -27,12 +25,10 @@ export default function ReposListPage() {
     setPage,
     importRepoIdentifier,
     setImportRepoIdentifier,
-    updateRepository,
     importToastId,
     setImportToastId
   } = useRepoStore()
-  // const [importToastId, setImportToastId] = useState<string | null>(null)
-  const { toast, dismiss, update } = useToast()
+  const { toast, dismiss } = useToast()
 
   const [query, setQuery] = useQueryState('query')
   const { queryPage } = usePaginationQueryStateWithStore({ page, setPage })
@@ -102,43 +98,10 @@ export default function ReposListPage() {
           </ToastAction>
         )
       })
-      console.log('importRepoIdentifier', importRepoIdentifier, importToastId, id)
 
       setImportToastId(id)
     }
   }, [importRepoIdentifier, setImportRepoIdentifier])
-
-  const onEvent = useCallback(
-    (eventData: RepoRepositoryOutput) => {
-      update({
-        id: importToastId ?? '',
-        title: 'Repository imported',
-        description: importRepoIdentifier,
-        duration: 5000,
-        action: (
-          <Link to={routes.toRepoSummary({ spaceId, repoId: importRepoIdentifier ?? '' })}>
-            <ToastAction altText="View repository">View</ToastAction>
-          </Link>
-        ),
-        variant: 'success'
-      })
-      const transformedRepo = transformRepoList([eventData])
-      updateRepository(transformedRepo[0])
-      // addRepository(transformedRepo[0])
-      setImportRepoIdentifier(null)
-      setImportToastId(null)
-    },
-    [importToastId]
-  )
-
-  const events = useMemo(() => [SSEEvent.REPO_IMPORTED], [])
-
-  useSpaceSSE({
-    space: spaceURL,
-    events,
-    onEvent,
-    shouldRun: true
-  })
 
   return (
     <>
@@ -154,7 +117,6 @@ export default function ReposListPage() {
         toCreateRepo={() => routes.toCreateRepo({ spaceId })}
         toImportRepo={() => routes.toImportRepo({ spaceId })}
       />
-      <Toaster />
     </>
   )
 }

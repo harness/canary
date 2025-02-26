ccab62a0c128a99e0efb98607b00b3a02d9fcba9
Vardan Bansal <vardan.bansal@harness.io>
2025-02-26 15:06:18 -0800

    Added few tests and removed duplicate util impl

--- packages/ui/src/utils/utils.ts
+++ packages/ui/src/utils/utils.ts
@@ -3,18 +3,6 @@ import { createElement, ReactNode } from 'react'
 import { TimeAgoHoverCard } from '@views/repo/components/time-ago-hover-card'
 import { formatDistance, formatDistanceToNow } from 'date-fns'
 
-export const getInitials = (name: string, length = 2) => {
-  // Split the name into an array of words, ignoring empty strings
-  const words = name.split(' ').filter(Boolean)
-
-  // Get the initials from the words
-  const initials = words
-    .map(word => word[0].toUpperCase()) // Get the first letter of each word
-    .join('')
-
-  // If length is provided, truncate the initials to the desired length
-  return length ? initials.slice(0, length) : initials
-}
 export const INITIAL_ZOOM_LEVEL = 1
 export const ZOOM_INC_DEC_LEVEL = 0.1
 

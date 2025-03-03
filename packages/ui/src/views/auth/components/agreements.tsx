4d84834683108eabcd9c450d891a59cf7e0515e1
Vardan Bansal <vardan.bansal@harness.io>
2025-03-03 12:08:37 -0800

    minor revert

--- packages/ui/src/views/auth/components/agreements.tsx
+++ packages/ui/src/views/auth/components/agreements.tsx
@@ -3,7 +3,7 @@ import { StyledLink } from '@/components'
 export function Agreements() {
   return (
     <span className="relative z-10 mt-auto text-center text-12 leading-tight text-foreground-5">
-      By joining, you agree to <StyledLink to="https://harness.io/subscriptionterms">Terms of Service</StyledLink>
+      By joining, you agree to <StyledLink to="https://harness.io/subscriptionterms">Terms of Service</StyledLink>{' '}
       and&nbsp;
       <StyledLink to="https://harness.io/privacy">Privacy Policy</StyledLink>
     </span>

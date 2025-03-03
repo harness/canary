d02f189b7e346032e354743f086b47a78142c74a
Srdjan Arsic <srdjan.arsic@harness.io>
2025-03-03 18:31:42 +0100

    fix edit flow

--- apps/design-system/src/subjects/views/unified-pipeline-studio/mocks/template-step-form.ts
+++ apps/design-system/src/subjects/views/unified-pipeline-studio/mocks/template-step-form.ts
@@ -3,17 +3,17 @@ import { IInputConfigWithConfig, InputType } from '@harnessio/ui/views'
 const inputs: IInputConfigWithConfig[] = [
   {
     inputType: InputType.text,
-    path: `with.testpath1`,
+    path: `template.with.testpath1`,
     label: 'Input 1'
   },
   {
     inputType: InputType.text,
-    path: `with.testpath2`,
+    path: `template.with.testpath2`,
     label: 'Input 2'
   },
   {
     inputType: InputType.text,
-    path: `with.testpath3`,
+    path: `template.with.testpath3`,
     label: 'Input 3'
   }
 ]

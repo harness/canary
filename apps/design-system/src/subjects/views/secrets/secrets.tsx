943709f0e4b2ececd38a66d492bd4cf61de1109e
sanskar <c_sanskar.sehgal@harness.io>
2025-03-08 17:27:55 -0800

    feat: add basic logic for renderer

--- apps/design-system/src/subjects/views/secrets/secrets.tsx
+++ apps/design-system/src/subjects/views/secrets/secrets.tsx
@@ -13,7 +13,7 @@ import {
   SecretType
 } from '@harnessio/ui/views'
 
-import { secretsData } from './secrets-data'
+import { getAccountsData, getOrgData, getProjectData, getSecretsData } from './secrets-data'
 
 export const SecretsPage = () => {
   const [selectedType, setSelectedType] = useState<SecretType>(SecretType.New)
@@ -21,6 +21,7 @@ export const SecretsPage = () => {
   // State for existing secrets
   const [selectedSecret, setSelectedSecret] = useState<SecretItem | null>(null)
   const [activeScope, setActiveScope] = useState<SecretScope | null>(null)
+  const [folders, setFolders] = useState<string[]>([])
 
   const onSubmit = (data: CreateSecretFormFields) => {
     console.log('Submitted data:', data)
@@ -35,6 +36,21 @@ export const SecretsPage = () => {
   const handleScopeChange = (scope: SecretScope) => {
     setActiveScope(scope)
     console.log('Scope changed to:', scope)
+  }
+
+  const handleFolderChange = (_folder: string, scope: SecretScope) => {
+    setActiveScope(scope)
+    switch (scope) {
+      case 'account':
+        setFolders(getOrgData().map(org => org.organizationResponse.organization.identifier) as any)
+        break
+      case 'organization':
+        setFolders(getProjectData().map(project => project.projectResponse.project.identifier) as any)
+        break
+      case 'project':
+        setFolders([])
+        break
+    }
   }
 
   const handleCancel = () => {
@@ -56,7 +72,7 @@ export const SecretsPage = () => {
       case SecretType.Existing:
         return (
           <ExistingSecrets
-            secretsData={secretsData.map(secret => ({
+            secretsData={getSecretsData().map(secret => ({
               ...secret,
               id: secret.secret.identifier,
               name: secret.secret.name
@@ -66,6 +82,8 @@ export const SecretsPage = () => {
             onSelectEntity={handleSelectSecret}
             onScopeChange={handleScopeChange}
             onCancel={handleCancel}
+            onFolderChange={handleFolderChange}
+            folders={folders}
           />
         )
       default:

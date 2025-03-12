597dd8e95e187962e514ca2edb08ce7d2e5b7de2
Srdjan Arsic <srdjan.arsic@harness.io>
2025-03-12 19:17:33 +0100

    fix

--- packages/pipeline-graph/src/index.ts
+++ packages/pipeline-graph/src/index.ts
@@ -3,6 +3,6 @@ export * from './types/nodes'
 export * from './types/nodes-internal'
 
 export * from './types/node-content'
-export type { ParallelContainerConfig, SerialContainerConfig } from './types/container-node'
+export { type ParallelContainerConfig, type SerialContainerConfig } from './types/container-node'
 export * from './context/canvas-provider'
 export { type CollapseButtonProps } from './components/components/collapse'

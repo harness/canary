0114f9915e3c613e8daed4fb60eb310a52237352
Pranesh <pranesh.g@harness.io>
2025-03-12 16:35:32 +0530

    Add js tokens build

---
+++ packages/ui/export-tokens-js/constants.js
@@ -0,0 +1,12 @@
+const DESIGN_SYSTEM_ROOT = 'src/styles-js/design-system'
+
+export const STYLE_FILE_PATH = {
+  DESIGN_SYSTEM: DESIGN_SYSTEM_ROOT,
+  INDEX: `${DESIGN_SYSTEM_ROOT}/index.js`,
+  CORE: `${DESIGN_SYSTEM_ROOT}/core.js`,
+  COLORS: `${DESIGN_SYSTEM_ROOT}/colors.js`,
+  BREAKPOINT: `${DESIGN_SYSTEM_ROOT}/breakpoint.js`,
+  COMPONENTS: `${DESIGN_SYSTEM_ROOT}/components.js`
+}
+
+export const DESIGN_SYSTEM_PREFIX = 'canary'

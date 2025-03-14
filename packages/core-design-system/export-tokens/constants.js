3e362d5cdee696ad6109467085b4e732c67f8409
Pranesh <pranesh.g@harness.io>
2025-03-14 17:40:59 +0530

    Add core-design-system package

---
+++ packages/core-design-system/export-tokens/constants.js
@@ -0,0 +1,12 @@
+const DESIGN_SYSTEM_ROOT = 'src/styles'
+
+export const STYLE_FILE_PATH = {
+  DESIGN_SYSTEM: DESIGN_SYSTEM_ROOT,
+  INDEX: `${DESIGN_SYSTEM_ROOT}/index.css`,
+  CORE: `${DESIGN_SYSTEM_ROOT}/core.css`,
+  COLORS: `${DESIGN_SYSTEM_ROOT}/colors.css`,
+  BREAKPOINT: `${DESIGN_SYSTEM_ROOT}/breakpoint.css`,
+  COMPONENTS: `${DESIGN_SYSTEM_ROOT}/components.css`
+}
+
+export const DESIGN_SYSTEM_PREFIX = 'canary'

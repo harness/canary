6309939c195e403710df2db8b5f1a50c9191ac62
Abhinav Rastogi <abhinav.rastogi@harness.io>
2025-03-14 13:06:57 -0700

    fix prettier

--- packages/ui/src/views/empty-page.tsx
+++ packages/ui/src/views/empty-page.tsx
@@ -7,11 +7,7 @@ export const EmptyPage = ({ pathName, comingSoon }: { pathName: string; comingSo
     <div className="flex min-h-screen items-center">
       <NoData
         iconName={comingSoon ? 'no-data-cog' : 'no-search-magnifying-glass'}
-        title={
-          comingSoon
-            ? `Coming soon`
-            : `Upgrade to Harness Enterprise to access ${pathName}`
-        }
+        title={comingSoon ? 'Coming soon' : `Upgrade to Harness Enterprise to access ${pathName}`}
         description={[]}
         primaryButton={{
           label: 'Take me back',

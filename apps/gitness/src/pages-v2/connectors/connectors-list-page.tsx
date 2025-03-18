ce82f19832559f061d87f258869845da3b2014af
Calvin Lee <calvin.lee@harness.io>
2025-03-18 17:24:28 -0600

    feat: fix checks
    Signed-off-by: Calvin Lee <calvin.lee@harness.io>

--- apps/gitness/src/pages-v2/connectors/connectors-list-page.tsx
+++
@@ -1,6 +0,0 @@
-import { ConnectorsListPageView } from '@harnessio/ui/views'
-
-// temp component for testing in standalone
-export default function ConnectorsListPage() {
-  return <ConnectorsListPageView />
-}

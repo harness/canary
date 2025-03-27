8cc93374cb45c09c24b6bec8ece3f8f67deaf24a
Vardan Bansal <vardan.bansal@harness.io>
2025-03-27 10:02:35 -0700

    cleanup - remove fallback on "Icon"

--- packages/ui/src/components/logo/logo.tsx
+++ packages/ui/src/components/logo/logo.tsx
@@ -12,8 +12,6 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ name, size = 24 }) => {
   const icon = LogoNameMap[name]
 
-  if (!icon) return <Icon name="connectors" size={size} />
-
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d={icon.path} fill={`#${icon.hex}`} />

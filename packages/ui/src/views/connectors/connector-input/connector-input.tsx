76e734469b356fa25340f31e7fa0a7c341e3894d
sanskar <c_sanskar.sehgal@harness.io>
2025-03-27 12:53:55 -0700

    fix: review comments

--- packages/ui/src/views/connectors/connector-input/connector-input.tsx
+++ packages/ui/src/views/connectors/connector-input/connector-input.tsx
@@ -3,27 +3,6 @@ import { InputReferenceProps } from '@views/platform/input-reference-component'
 
 import { ConnectorItem } from '../types'
 
-export const ConnectorInput = ({
-  value,
-  onClick,
-  onEdit,
-  onClear,
-  className,
-  placeholder,
-  label,
-  ...props
-}: Omit<InputReferenceProps<ConnectorItem>, 'icon'>) => {
-  return (
-    <InputReference<ConnectorItem>
-      placeholder={placeholder}
-      value={value}
-      label={label}
-      icon="connectors"
-      onClick={onClick}
-      onEdit={onEdit}
-      onClear={onClear}
-      className={className}
-      {...props}
-    />
-  )
+export const ConnectorInput = ({ ...props }: Omit<InputReferenceProps<ConnectorItem>, 'icon'>) => {
+  return <InputReference<ConnectorItem> icon="connectors" {...props} />
 }

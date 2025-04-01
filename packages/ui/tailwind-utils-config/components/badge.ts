55f85b4bd63063d3271142c884166b44ad27816a
Pranesh <pranesh.g@harness.io>
2025-04-01 11:33:13 +0530

    Add badge status component

--- packages/ui/tailwind-utils-config/components/badge.ts
+++ packages/ui/tailwind-utils-config/components/badge.ts
@@ -1,7 +1,7 @@
 import { CSSRuleObject } from 'tailwindcss/types/config'
 
 /** Variants */
-const variants = ['solid', 'soft', 'surface']
+const variants = ['solid', 'soft', 'surface', 'status']
 
 /**
  *  Themes
@@ -29,6 +29,7 @@ function createBadgeVariantStyles() {
   // Exclude "ai" theme from variants
   const aiFilteredThemes = themes.filter(theme => theme !== 'ai')
   const combinationStyles: CSSRuleObject = {}
+  const statusCircleStyles: CSSRuleObject = {}
 
   variants.forEach(variant => {
     aiFilteredThemes.forEach(theme => {
@@ -36,25 +37,62 @@ function createBadgeVariantStyles() {
 
       const themeStyle = themeStyleMapper[theme as keyof typeof themeStyleMapper]
 
-      style[`backgroundColor`] = `var(--cn-set-${themeStyle}-${variant}-bg)`
-      style[`color`] = `var(--cn-set-${themeStyle}-${variant}-text)`
-      style[`borderColor`] = `var(--cn-set-${themeStyle}-${variant}-border)`
+      /**
+       * Status variant don't need background, color and border
+       */
+      if (variant !== 'status') {
+        style[`backgroundColor`] = `var(--cn-set-${themeStyle}-${variant}-bg)`
+        style[`color`] = `var(--cn-set-${themeStyle}-${variant}-text)`
+        style[`borderColor`] = `var(--cn-set-${themeStyle}-${variant}-border)`
 
-      combinationStyles[`&.badge-${variant}.badge-${theme}`] = style
+        combinationStyles[`&:where(.badge-${variant}.badge-${theme})`] = style
+      } else {
+        // Add status circle styles for each theme
+        statusCircleStyles[`&.badge-status.badge-${theme}::before`] = {
+          backgroundColor: `var(--cn-set-${themeStyle}-solid-bg)`
+        }
+      }
     })
   })
 
-  return combinationStyles
+  return { ...combinationStyles, ...statusCircleStyles }
 }
 
 export default {
   '.badge': {
-    padding: 'var(--cn-badge-status-py) var(--cn-badge-default-px)',
-    gap: 'var(--cn-badge-default-gap)',
     height: 'var(--cn-badge-size-default)',
-    borderRadius: 'var(--cn-badge-radius)',
-    border: '1px solid var(--cn-set-gray-solid-border)',
     '@apply select-none pointer-events-none': '',
+
+    '&:where(:not(.badge-status))': {
+      borderRadius: 'var(--cn-badge-radius)',
+      border: '1px solid var(--cn-set-gray-solid-border)',
+      gap: 'var(--cn-badge-default-gap)',
+      padding: 'var(--cn-badge-status-py) var(--cn-badge-default-px)'
+    },
+
+    '&:where(.badge-status)': {
+      gap: 'var(--cn-badge-status-gap)',
+      padding: 'var(--cn-badge-status-py) var(--cn-badge-status-px)',
+      position: 'relative',
+      // paddingLeft: 'calc(var(--cn-badge-status-py) + 8px + var(--cn-badge-status-gap))',
+      paddingLeft: 'calc(var(--cn-badge-status-px) + 6px + var(--cn-badge-status-gap))',
+
+      '&.badge-sm': {
+        paddingLeft: 'calc(10px + var(--cn-badge-status-px))'
+      },
+
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 'var(--cn-badge-status-py)',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        width: '8px',
+        height: '8px',
+        borderRadius: '50%',
+        backgroundColor: 'var(--cn-set-gray-solid-bg)' // Default color
+      }
+    },
 
     /** Size */
     '&-sm': {

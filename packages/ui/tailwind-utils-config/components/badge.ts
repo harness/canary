3eb9bc6f7f46961fcd05c3db9c01d0e3c6fdc4c0
Pranesh <pranesh.g@harness.io>
2025-04-02 11:12:43 +0530

    Add counter badge variant

--- packages/ui/tailwind-utils-config/components/badge.ts
+++ packages/ui/tailwind-utils-config/components/badge.ts
@@ -1,7 +1,7 @@
 import { CSSRuleObject } from 'tailwindcss/types/config'
 
 /** Variants */
-const variants = ['solid', 'soft', 'surface', 'status']
+const variants = ['solid', 'soft', 'surface', 'status', 'counter'] as const
 
 /**
  *  Themes
@@ -26,12 +26,16 @@ const themeStyleMapper: Record<Exclude<(typeof themes)[number], 'ai'>, string> =
 }
 
 function createBadgeVariantStyles() {
-  // Exclude "ai" theme from variants
+  // Exclude "ai" theme from themes
   const aiFilteredThemes = themes.filter(theme => theme !== 'ai')
+
+  // Exclude "counter" variant from variants
+  const counterFilteredVariants = variants.filter(variant => variant !== 'counter')
+
   const combinationStyles: CSSRuleObject = {}
   const statusCircleStyles: CSSRuleObject = {}
 
-  variants.forEach(variant => {
+  counterFilteredVariants.forEach(variant => {
     aiFilteredThemes.forEach(theme => {
       const style: CSSRuleObject = {}
 
@@ -61,49 +65,47 @@ function createBadgeVariantStyles() {
 export default {
   '.badge': {
     height: 'var(--cn-badge-size-default)',
+    padding: '0 var(--cn-badge-default-px)',
     '@apply select-none pointer-events-none': '',
 
-    '&:where(:not(.badge-status))': {
-      borderRadius: 'var(--cn-badge-radius)',
-      border: '1px solid var(--cn-set-gray-solid-border)',
-      gap: 'var(--cn-badge-default-gap)',
-      padding: 'var(--cn-badge-status-py) var(--cn-badge-default-px)'
-    },
-
-    '&:where(.badge-status)': {
-      gap: 'var(--cn-badge-status-gap)',
-      padding: 'var(--cn-badge-status-py) var(--cn-badge-status-px)',
-      position: 'relative',
-      // paddingLeft: 'calc(var(--cn-badge-status-py) + 8px + var(--cn-badge-status-gap))',
-      paddingLeft: 'calc(var(--cn-badge-status-px) + 6px + var(--cn-badge-status-gap))',
-
-      '&.badge-sm': {
-        paddingLeft: 'calc(10px + var(--cn-badge-status-px))'
-      },
-
-      '> .badge-indicator': {
-        backgroundColor: 'var(--cn-set-gray-solid-bg)'
-      }
-
-      // '&::before': {
-      //   content: '""',
-      //   position: 'absolute',
-      //   left: 'var(--cn-badge-status-py)',
-      //   top: '50%',
-      //   transform: 'translateY(-50%)',
-      //   width: '8px',
-      //   height: '8px',
-      //   borderRadius: '50%',
-      //   backgroundColor: 'var(--cn-set-gray-solid-bg)' // Default color
-      // }
-    },
-
     /** Size */
-    '&-sm': {
+    '&:where(.badge-sm)': {
       height: 'var(--cn-badge-size-sm)',
       gap: 'var(--cn-badge-sm-gap)',
       padding: 'var(--cn-badge-sm-px)',
       '@apply font-caption-soft': ''
+    },
+
+    '&:where(:not(.badge-status))': {
+      borderRadius: 'var(--cn-badge-radius)',
+      border: 'var(--cn-badge-border) solid var(--cn-set-gray-solid-border)',
+      gap: 'var(--cn-badge-default-gap)'
+    },
+
+    '&:where(.badge-counter)': {
+      color: 'var(--cn-set-gray-surface-text)',
+      borderRadius: 'var(--cn-badge-counter-radius)',
+      borderColor: 'var(--cn-set-gray-surface-border)',
+      backgroundColor: 'var(--cn-set-gray-surface-bg)',
+      height: 'var(--cn-badge-counter-size-default)',
+      padding: 'var(--cn-badge-counter-py) var(--cn-badge-counter-px)',
+
+      '&.badge-primary': {
+        backgroundColor: 'var(--cn-set-brand-solid-bg)',
+        color: 'var(--cn-set-brand-solid-text)',
+        borderColor: 'var(--cn-set-brand-solid-border)'
+      }
+    },
+
+    '&:where(.badge-status)': {
+      gap: 'var(--cn-badge-status-gap)',
+      padding: '0',
+
+      '> .badge-indicator': {
+        width: 'var(--cn-badge-indicator-size-default)',
+        height: 'var(--cn-badge-indicator-size-default)',
+        backgroundColor: 'var(--cn-set-gray-solid-bg)'
+      }
     },
 
     /**
@@ -116,7 +118,7 @@ export default {
       backgroundImage: `linear-gradient(to right, var(--cn-set-ai-surface-bg), var(--cn-set-ai-surface-bg)), var(--cn-set-ai-surface-border)`,
       backgroundOrigin: 'border-box',
       backgroundClip: 'padding-box, border-box',
-      border: '1px solid transparent'
+      border: 'var(--cn-badge-border) solid transparent'
     },
 
     /** Variants */

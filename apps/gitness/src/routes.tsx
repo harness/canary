48dbf6d287e289644c909e9ab6e2f33c4fead1f8
Srdjan Arsic <srdjan.arsic@harness.io>
2025-04-03 18:10:51 +0200

    revert routes

--- apps/gitness/src/routes.tsx
+++ apps/gitness/src/routes.tsx
@@ -45,6 +45,7 @@ import RepoCommitDetailsPage from './pages-v2/repo/repo-commit-details'
 import { CommitDiffContainer } from './pages-v2/repo/repo-commit-details-diff'
 import RepoCommitsPage from './pages-v2/repo/repo-commits'
 import { CreateRepo } from './pages-v2/repo/repo-create-page'
+import RepoExecutionListPage from './pages-v2/repo/repo-execution-list'
 import { ImportMultipleRepos } from './pages-v2/repo/repo-import-multiple-container'
 import { ImportRepo } from './pages-v2/repo/repo-import-page'
 import RepoLayout from './pages-v2/repo/repo-layout'
@@ -71,6 +72,7 @@ enum Page {
   Conversation = 'Conversation',
   Changes = 'Changes',
   Checks = 'Checks',
+  Pipelines = 'Pipelines',
   Executions = 'Executions',
   Settings = 'Settings',
   Branch_Rules = 'Branch Rules',
@@ -367,7 +369,62 @@ export const repoRoutes: CustomRouteObject[] = [
               }
             ]
           },
-
+          {
+            path: 'pipelines',
+            handle: {
+              breadcrumb: () => <span>{Page.Pipelines}</span>
+            },
+            children: [
+              {
+                index: true,
+                element: <>RepoPipelineListPage</>,
+                handle: {
+                  pageTitle: Page.Pipelines
+                }
+              },
+              {
+                path: ':pipelineId',
+                handle: {
+                  breadcrumb: ({ pipelineId }: { pipelineId: string }) => <span>{pipelineId}</span>
+                },
+                children: [
+                  {
+                    index: true,
+                    element: <RepoExecutionListPage />,
+                    handle: {
+                      breadcrumb: () => <span>{Page.Executions}</span>,
+                      pageTitle: Page.Executions
+                    }
+                  },
+                  {
+                    path: 'edit',
+                    element: <>PipelineEditPage</>,
+                    handle: {
+                      breadcrumb: () => <span>Edit</span>,
+                      routeName: RouteConstants.toPipelineEdit
+                    }
+                  },
+                  {
+                    path: 'executions',
+                    handle: {
+                      routeName: RouteConstants.toExecutions
+                    },
+                    children: [
+                      { index: true, element: <RepoExecutionListPage />, handle: { pageTitle: Page.Executions } },
+                      {
+                        path: ':executionId',
+                        element: <>Execution Details Page</>,
+                        handle: {
+                          breadcrumb: ({ executionId }: { executionId: string }) => <span>{executionId}</span>,
+                          routeName: RouteConstants.toExecution
+                        }
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
           {
             path: 'settings',
             element: <RepoSettingsLayout useTranslationStore={useTranslationStore} />,
@@ -564,6 +621,15 @@ export const repoRoutes: CustomRouteObject[] = [
     ]
   },
   {
+    path: 'pipelines',
+    element: <>ProjectPipelineListPage</>,
+    handle: {
+      breadcrumb: () => <span>{Page.Pipelines}</span>,
+      pageTitle: Page.Pipelines
+    },
+    children: []
+  },
+  {
     path: 'search',
     element: <EmptyPage pathName="Search" comingSoon={true} />,
     handle: {
@@ -624,6 +690,19 @@ export const routes: CustomRouteObject[] = [
           breadcrumb: () => <span>{Page.Repositories}</span>,
           routeName: RouteConstants.toRepositories,
           pageTitle: Page.Repositories
+        }
+      },
+      {
+        path: 'pipelines',
+        element: (
+          <SandboxLayout.Main>
+            <h1>Pipelines</h1>
+          </SandboxLayout.Main>
+        ),
+        handle: {
+          breadcrumb: () => <span>{Page.Pipelines}</span>,
+          routeName: RouteConstants.toPipelines,
+          pageTitle: Page.Pipelines
         }
       },
       {

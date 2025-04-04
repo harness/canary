908b44911eff80908ad34398c614874984e0eab4
Andrew Koreykin <ankormoreankor@gmail.com>
2025-04-04 17:41:38 +0400

    fixes

--- packages/ui/src/views/layouts/PullRequestLayout.tsx
+++ packages/ui/src/views/layouts/PullRequestLayout.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren, ReactNode } from 'react'
+import { NavLinkProps } from 'react-router-dom'
 
-import { Badge, Icon, IconProps, Tabs } from '@/components'
+import { Badge, Icon, IconProps } from '@/components'
 import { useRouterContext, useTheme } from '@/context'
 import { cn } from '@/utils'
 import { SandboxLayout } from '@views/layouts/SandboxLayout'
@@ -15,7 +16,7 @@ const TabTitleWithIcon = ({
 }: PropsWithChildren<{ icon: IconProps['name']; badgeContent?: ReactNode }>) => (
   <>
     <div className="flex items-center gap-x-1">
-      <Icon className="text-icons-1 group-data-[state=active]:text-icons-2" size={14} name={icon} />
+      <Icon className="text-icons-1 group-[.is-active]:text-icons-2" size={14} name={icon} />
       {children}
     </div>
     {!!badgeContent && (
@@ -25,6 +26,23 @@ const TabTitleWithIcon = ({
     )}
   </>
 )
+
+const PullRequestTabItem: FC<NavLinkProps> = ({ className, ...props }) => {
+  const { NavLink } = useRouterContext()
+
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        cn(
+          'group relative z-[1] text-foreground-2 hover:text-foreground-1 inline-flex h-9 items-center justify-center gap-x-1.5 whitespace-nowrap rounded-t-md border-x border-t border-transparent px-3.5 py-1 font-normal transition-all focus-visible:duration-0',
+          { 'border-borders-1 bg-background-1 text-foreground-1 is-active': isActive },
+          className
+        )
+      }
+      {...props}
+    />
+  )
+}
 
 interface PullRequestLayoutProps {
   usePullRequestStore: () => IPullRequestStore
@@ -47,12 +65,10 @@ export const PullRequestLayout: FC<PullRequestLayoutProps> = ({
   repoId,
   updateTitle
 }) => {
-  const { Outlet, NavLink, location } = useRouterContext()
+  const { Outlet } = useRouterContext()
   const { pullRequest } = usePullRequestStore()
   const { t } = useTranslationStore()
   const { isInset } = useTheme()
-
-  const activeTab = (location.pathname.split('/').pop() as PullRequestTabsKeys) ?? PullRequestTabsKeys.CONVERSATION
 
   return (
     <SandboxLayout.Main fullWidth>
@@ -61,7 +77,41 @@ export const PullRequestLayout: FC<PullRequestLayoutProps> = ({
           <PullRequestHeader className="mb-10" updateTitle={updateTitle} data={{ ...pullRequest, spaceId, repoId }} />
         )}
 
-        <Tabs.Root variant="tabnav" className="mb-7" defaultValue={activeTab}>
+        <nav
+          className={cn(
+            'relative before:bottom-0 before:absolute before:w-[calc(100vw-var(--sidebar-width))] before:min-w-[calc(100%+3rem)] before:left-1/2 before:-translate-x-1/2 before:border-b before:border-borders-4',
+            { 'before:w-[calc(100vw-var(--sidebar-width)-6px)]': isInset }
+          )}
+        >
+          <ul className="mb-7 flex items-center">
+            <li>
+              <PullRequestTabItem to={PullRequestTabsKeys.CONVERSATION}>
+                <TabTitleWithIcon
+                  icon="comments"
+                  badgeContent={!!pullRequest?.stats?.conversations && pullRequest?.stats?.conversations}
+                >
+                  {t('views:pullRequests.conversation', 'Conversation')}
+                </TabTitleWithIcon>
+              </PullRequestTabItem>
+            </li>
+            <li>
+              <PullRequestTabItem to={PullRequestTabsKeys.COMMITS}>
+                <TabTitleWithIcon icon="tube-sign" badgeContent={pullRequest?.stats?.commits}>
+                  {t('views:pullRequests.commits', 'Commits')}
+                </TabTitleWithIcon>
+              </PullRequestTabItem>
+            </li>
+            <li>
+              <PullRequestTabItem to={PullRequestTabsKeys.CHANGES}>
+                <TabTitleWithIcon icon="changes" badgeContent={pullRequest?.stats?.files_changed}>
+                  {t('views:pullRequests.changes', 'Changes')}
+                </TabTitleWithIcon>
+              </PullRequestTabItem>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Tabs.Root variant="tabnav" className="mb-7" defaultValue={activeTab}>
           <Tabs.List
             className={cn(
               'before:w-[calc(100vw-var(--sidebar-width))] before:min-w-[calc(100%+3rem)] before:left-1/2 before:-translate-x-1/2 before:bg-border',
@@ -93,7 +143,7 @@ export const PullRequestLayout: FC<PullRequestLayoutProps> = ({
               </NavLink>
             </Tabs.Trigger>
           </Tabs.List>
-        </Tabs.Root>
+        </Tabs.Root> */}
 
         <Outlet />
       </SandboxLayout.Content>

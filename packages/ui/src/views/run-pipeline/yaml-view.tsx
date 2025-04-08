0b9b6d4dd28c4d64b1bcef22422ef679a6c2f99e
Srdjan Arsic <srdjan.arsic@harness.io>
2025-04-08 16:31:21 +0200

    add run pipeline drawer preview to design-system

--- packages/ui/src/views/run-pipeline/yaml-view.tsx
+++ packages/ui/src/views/run-pipeline/yaml-view.tsx
@@ -1,5 +1,7 @@
 import { YamlEditor, YamlRevision } from '@harnessio/yaml-editor'
 
+import { monacoThemes } from '..'
+
 export interface YamlViewProps {
   yamlRevision: YamlRevision
   onYamlRevisionChange: (yamlRevision: YamlRevision) => void
@@ -9,11 +11,16 @@ export interface YamlViewProps {
 export default function YamlView(props: YamlViewProps) {
   const { yamlRevision, onYamlRevisionChange, theme } = props
 
+  console.log(theme)
   return (
     <div style={{ height: '300px' }}>
       <YamlEditor
         inlineActions={[]}
         theme={theme}
+        themeConfig={{
+          defaultTheme: 'dark',
+          themes: monacoThemes
+        }}
         yamlRevision={yamlRevision}
         onYamlRevisionChange={revision => {
           onYamlRevisionChange(revision!)

8970620967d8cdf5d94626d2583660166477d9bf
Vardan Bansal <vardan.bansal@harness.io>
2025-04-08 01:12:54 -0700

    cleanup: clean up code to render menu items

--- apps/gitness/src/components-v2/mfe/side-bar.tsx
+++ apps/gitness/src/components-v2/mfe/side-bar.tsx
@@ -13,6 +13,23 @@ const AppSidebar: FC = () => {
   const { t } = useTranslationStore()
   const { NavLink } = useRouterContext()
   const collapsed = false
+
+  const renderMenuItem = (to: string, text: string) => (
+    <Sidebar.MenuItem>
+      <NavLink className="block" to={to} end>
+        {({ isActive }) => (
+          <Sidebar.MenuButton asChild isActive={isActive}>
+            <Sidebar.MenuItemText
+              text={text}
+              icon={<Icon name="repositories-gradient" size={14} />}
+              active={isActive}
+            />
+          </Sidebar.MenuButton>
+        )}
+      </NavLink>
+    </Sidebar.MenuItem>
+  )
+
   return (
     <Sidebar.Provider className="min-h-svh">
       <Sidebar.Root className="fixed z-20 h-svh">
@@ -25,109 +42,23 @@ const AppSidebar: FC = () => {
           <Sidebar.Group>
             <Sidebar.GroupContent>
               <Sidebar.Menu>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/repos'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Repositories'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/search'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Search'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/pull-requests'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Pull Requests'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/manage-repos'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Manage Repositories'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
+                {renderMenuItem('/repos', 'Repositories')}
+                {renderMenuItem('/search', 'Search')}
+                {renderMenuItem('/pull-requests', 'Pull Requests')}
+                {renderMenuItem('/manage-repos', 'Manage Repositories')}
               </Sidebar.Menu>
             </Sidebar.GroupContent>
           </Sidebar.Group>
           <Sidebar.Group className="border-t">
             <Sidebar.GroupContent>
-              <Sidebar.Menu>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/project-settings'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Project Settings'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
-              </Sidebar.Menu>
+              <Sidebar.Menu>{renderMenuItem('/project-settings', 'Project Settings')}</Sidebar.Menu>
             </Sidebar.GroupContent>
           </Sidebar.Group>
           <Sidebar.Group className="border-t">
             <Sidebar.GroupContent>
               <Sidebar.Menu>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/account-settings'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Account Settings'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
-                <Sidebar.MenuItem>
-                  <NavLink className="block" to={'/account-settings'} end>
-                    {({ isActive }) => (
-                      <Sidebar.MenuButton asChild isActive={isActive}>
-                        <Sidebar.MenuItemText
-                          text={'Organization Settings'}
-                          icon={<Icon name={'repositories-gradient'} size={14} />}
-                          active={isActive}
-                        />
-                      </Sidebar.MenuButton>
-                    )}
-                  </NavLink>
-                </Sidebar.MenuItem>
+                {renderMenuItem('/account-settings', 'Account Settings')}
+                {renderMenuItem('/organization-settings', 'Organization Settings')}
               </Sidebar.Menu>
             </Sidebar.GroupContent>
           </Sidebar.Group>

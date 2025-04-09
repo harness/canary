d40045cdf31b45828047681537fd7ba0cca9cf2c
Vivek <c_vivek.patel@harness.io>
2025-04-09 19:35:05 +0530

    replaced bg-border with bg-cn-borders-3

--- packages/ui/src/components/app-sidebar/sidebar-search/command-palette.tsx
+++ packages/ui/src/components/app-sidebar/sidebar-search/command-palette.tsx
@@ -123,7 +123,7 @@ const Separator = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
 >(({ className, ...props }, ref) => (
-  <CommandPrimitive.Separator ref={ref} className={cn('-mx-1 h-px bg-border', className)} {...props} />
+  <CommandPrimitive.Separator ref={ref} className={cn('-mx-1 h-px bg-cn-borders-3', className)} {...props} />
 ))
 Separator.displayName = 'Separator'
 

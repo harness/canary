c4394ff5937ddea5a8ea2a60a277c8cb68072a8b
Shaurya Kalia <shaurya.kalia@harness.io>
2025-04-10 20:03:34 +0530

    fix: add tooltips for form inputs

--- packages/ui/src/views/unified-pipeline-studio/components/form-inputs/common/InputTooltip.tsx
+++ packages/ui/src/views/unified-pipeline-studio/components/form-inputs/common/InputTooltip.tsx
@@ -8,8 +8,10 @@ function InputTooltip(props: InputTooltipProps): JSX.Element | null {
   const { tooltip } = props
   return (
     <div className="w-full pt-2">
-      <div className="flex items-center gap-2 border rounded-md bg-cn-background-2 bg-gradient-to-b from-white/[0.04] to-white/0 p-2">
-        <Icon size={14} name="info-circle" className="text-cn-foreground-3" />
+      <div className="flex gap-2 border rounded-md bg-cn-background-2 bg-gradient-to-b from-white/[0.04] to-white/0 p-2">
+        <div className="pt-1">
+          <Icon size={14} name="info-circle" className="text-cn-foreground-3" />
+        </div>
         <span className="text-[12px] text-cn-foreground-1">{tooltip}</span>
       </div>
     </div>

3066623a37c761d6fd13be41259245430257d4ac
Vivek <c_vivek.patel@harness.io>
2025-04-11 19:26:12 +0530

    replaced --canary-widget-number-bg-gradient-to with --cn-gradient-pipeline-widget-blob-small-to

--- packages/ui/tailwind-design-system.ts
+++ packages/ui/tailwind-design-system.ts
@@ -388,7 +388,7 @@ export default {
         'widget-number-gradient':
           'linear-gradient(180deg, hsla(var(--cn-gradient-pipeline-widget-number-from)) 35.9%, hsla(var(--cn-gradient-pipeline-widget-number-to)) 100%)',
         'widget-number-bg-gradient':
-          'linear-gradient(135deg, hsla(var(--cn-gradient-pipeline-widget-blob-small-from)) 0%, hsla(var(--canary-widget-number-bg-gradient-to)) 67.67%)',
+          'linear-gradient(135deg, hsla(var(--cn-gradient-pipeline-widget-blob-small-from)) 0%, hsla(var(--cn-gradient-pipeline-widget-blob-small-to)) 67.67%)',
         'graph-gradient-1':
           'radial-gradient(88.57% 100% at 14.29% 0%, var(--canary-graph-gradient-bg-1) 10.62%, var(--canary-graph-gradient-bg-2) 75.86%)',
         'graph-bg-gradient': 'radial-gradient(circle, var(--canary-graph-viewport-bg) 1px, transparent 1px)'

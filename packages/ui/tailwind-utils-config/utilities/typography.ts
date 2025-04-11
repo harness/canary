46ba0af1080330c740bf89cd8cfa705617092c21
Pranesh <pranesh.g@harness.io>
2025-04-11 11:00:22 +0530

    Button component changes

--- packages/ui/tailwind-utils-config/utilities/typography.ts
+++ packages/ui/tailwind-utils-config/utilities/typography.ts
@@ -1,10 +1,20 @@
 export default {
   '.font': {
+    '&-body-strong': {
+      font: 'var(--cn-body-strong)'
+    },
+    '&-body-none-strong': {
+      font: 'var(--cn-body-none-strong)'
+    },
     '&-caption-soft': {
       font: 'var(--cn-caption-soft)'
     },
     '&-caption-normal': {
       font: 'var(--cn-caption-normal)'
+    },
+    // Update caption-none-normal name
+    '&-caption-none-normal': {
+      font: 'var(--cn-caption-none-normal)'
     }
   }
 }

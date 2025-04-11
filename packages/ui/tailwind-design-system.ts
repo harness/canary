4e30725966132679f56f598d81eb0178c8a1aeb0
Vivek <c_vivek.patel@harness.io>
2025-04-11 19:06:18 +0530

    replaced --canary-box-shadow-2 with -cn-shadow-color-2

--- packages/ui/tailwind-design-system.ts
+++ packages/ui/tailwind-design-system.ts
@@ -322,12 +322,12 @@ export default {
       },
       boxShadow: {
         1: '0px 8px 16px var(-cn-shadow-color-1)',
-        2: '0px 8px 8px var(--canary-box-shadow-2)',
+        2: '0px 8px 8px var(-cn-shadow-color-2)',
         'pagination-1': '0px 2px 4px var(--canary-box-shadow-pagination)',
         'as-border': 'inset 0 0 0 1px',
         'commit-list-bullet':
           '0px 0px 3px 0.5px hsla(var(--canary-background-05) / 0.2), 0px 0px 8px 1px hsla(var(--canary-background-05) / 0.3)',
-        auth: '0px 0px 20px var(--canary-box-shadow-2)'
+        auth: '0px 0px 20px var(-cn-shadow-color-2)'
       },
       borderColor: {
         DEFAULT: 'var(--cn-border-3)',

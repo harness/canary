045e6b4ed9c00f74ad3d8fcf9722aec7f854d619
Vivek <c_vivek.patel@harness.io>
2025-04-11 19:14:45 +0530

    replaced --canary-border-09 with --cn-border-1

--- packages/ui/tailwind-design-system.ts
+++ packages/ui/tailwind-design-system.ts
@@ -339,7 +339,7 @@ export default {
         'borders-6': 'hsl(var(--cn-border-1))',
         'borders-7': 'hsl(var(--cn-border-1))',
         'borders-8': 'hsl(var(--cn-border-2))',
-        'borders-9': 'hsl(var(--canary-border-09))',
+        'borders-9': 'hsl(var(--cn-border-1))',
         'borders-10': 'hsl(var(--canary-border-10))',
         'borders-danger': 'hsl(var(--canary-border-danger))',
         'borders-success': 'hsl(var(--canary-border-success))',

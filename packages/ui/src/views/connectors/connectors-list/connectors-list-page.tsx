f96b3394bb6f68f81a460c1fc6c1370c250a16f2
Vardan Bansal <vardan.bansal@harness.io>
2025-04-23 22:18:17 -0700

    remove redundant prop

--- packages/ui/src/views/connectors/connectors-list/connectors-list-page.tsx
+++ packages/ui/src/views/connectors/connectors-list/connectors-list-page.tsx
@@ -81,7 +81,7 @@ const ConnectorsListPage: FC<ConnectorListPageProps> = ({
   }
 
   return (
-    <SandboxLayout.Main fullWidth>
+    <SandboxLayout.Main>
       <SandboxLayout.Content className={cn({ 'h-full': !isLoading && !connectors.length && !searchQuery })}>
         <h1 className="text-6 text-cn-foreground-1 font-medium leading-snug tracking-tight">Connectors</h1>
         <Spacer size={6} />

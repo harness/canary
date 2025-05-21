b293b5a5decc581a3628d8e0a60b5988d025c1e2
Pranesh <pranesh.g@harness.io>
2025-05-21 16:18:36 +0530

    Update docs

--- apps/portal/src/components/docs-page/component-example.tsx
+++ apps/portal/src/components/docs-page/component-example.tsx
@@ -1,7 +1,9 @@
 import { type FC, useMemo } from "react";
-
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as components from "@harnessio/ui/components";
 import Example, { type ExampleProps } from "./example";
+import { useForm } from "react-hook-form";
 
 export type ComponentExampleProps = Omit<ExampleProps, "scope"> & {
   scope?: ExampleProps["scope"];
@@ -9,7 +11,7 @@ export type ComponentExampleProps = Omit<ExampleProps, "scope"> & {
 
 const ComponentExample: FC<ComponentExampleProps> = ({ code, scope }) => {
   const combinedScope = useMemo<ExampleProps["scope"]>(
-    () => ({ ...components, ...scope }),
+    () => ({ ...components, ...scope, zodResolver, z, useForm }),
     [scope],
   );
 

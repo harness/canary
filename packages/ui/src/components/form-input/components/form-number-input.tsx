d33de8f45bb3391bc9ca9b4a0621c9729e65d89d
Pranesh <pranesh.g@harness.io>
2025-05-21 12:34:03 +0530

    Fix lint error

--- packages/ui/src/components/form-input/components/form-number-input.tsx
+++ packages/ui/src/components/form-input/components/form-number-input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef } from 'react'
+import { forwardRef } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
 
 import { NumberInput, type NumberInputProps } from '@components/inputs'

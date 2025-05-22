d921b1be211533a6480ff24976225db2372a766e
Pranesh <pranesh.g@harness.io>
2025-05-22 11:05:25 +0530

    Refactor portal example component

--- apps/portal/src/components/docs-page/component-example.tsx
+++ apps/portal/src/components/docs-page/component-example.tsx
@@ -1,9 +1,6 @@
 import { type FC, useMemo } from "react";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import * as components from "@harnessio/ui/components";
 import Example, { type ExampleProps } from "./example";
-import { useForm } from "react-hook-form";
 
 export type ComponentExampleProps = Omit<ExampleProps, "scope"> & {
   scope?: ExampleProps["scope"];
@@ -11,7 +8,7 @@ export type ComponentExampleProps = Omit<ExampleProps, "scope"> & {
 
 const ComponentExample: FC<ComponentExampleProps> = ({ code, scope }) => {
   const combinedScope = useMemo<ExampleProps["scope"]>(
-    () => ({ ...components, ...scope, zodResolver, z, useForm }),
+    () => ({ ...components, ...scope }),
     [scope],
   );
 

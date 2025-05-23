56ea1691481edcb6f4d5b06bfbf1dede1a3e81a8
Vivek <c_vivek.patel@harness.io>
2025-05-23 20:06:12 +0530

    remove old Dialog component

--- packages/ui/src/components/app-sidebar/sidebar-search/command-palette.tsx
+++ packages/ui/src/components/app-sidebar/sidebar-search/command-palette.tsx
@@ -1,7 +1,6 @@
 import React, { PropsWithChildren } from 'react'
 
-import { Dialog as RadixDialog } from '@components/dialog'
-import { Overlay } from '@radix-ui/react-dialog'
+import { ModalDialog } from '@components/modal-dialog'
 import { cn } from '@utils/cn'
 import { Command as CommandPrimitive } from 'cmdk'
 
@@ -24,30 +23,24 @@ const Root = React.forwardRef<
 Root.displayName = 'Root'
 
 const Dialog = ({ children, open, onOpenChange }: PropsWithChildren<DialogProps>) => (
-  <RadixDialog.Root open={open} onOpenChange={onOpenChange}>
-    <RadixDialog.Content isShowCloseIcon={false}>
+  <ModalDialog.Root open={open} onOpenChange={onOpenChange}>
+    <ModalDialog.Content hideClose>
       <Root className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-cn-foreground-3 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2.5 [&_[cmdk-item]]:py-1.5 [&_[cmdk-item]_svg]:h-auto [&_[cmdk-item]_svg]:w-5">
         {children}
       </Root>
-    </RadixDialog.Content>
-  </RadixDialog.Root>
+    </ModalDialog.Content>
+  </ModalDialog.Root>
 )
 Dialog.displayName = 'Dialog'
 
 const Dropdown = ({ children, className, open, onOpenChange }: PropsWithChildren<DialogProps>) => (
-  <RadixDialog.Root open={open} onOpenChange={onOpenChange}>
-    <Overlay
-      className={cn(
-        'fixed inset-0 z-50 bg-cn-background-1/30 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-        className
-      )}
-    />
-    <RadixDialog.Content className="absolute left-6 top-[58px] z-50 grid w-full max-w-[300px] gap-4 rounded-[10px] border bg-cn-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg">
+  <ModalDialog.Root open={open} onOpenChange={onOpenChange}>
+    <ModalDialog.Content className="absolute left-6 top-[58px] z-50 grid w-full max-w-[300px] gap-4 rounded-[10px] border bg-cn-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg">
       <Root className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-cn-foreground-3 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2.5 [&_[cmdk-item]]:py-1.5 [&_[cmdk-item]_svg]:h-auto [&_[cmdk-item]_svg]:w-5">
         {children}
       </Root>
-    </RadixDialog.Content>
-  </RadixDialog.Root>
+    </ModalDialog.Content>
+  </ModalDialog.Root>
 )
 Dropdown.displayName = 'Dropdown'
 

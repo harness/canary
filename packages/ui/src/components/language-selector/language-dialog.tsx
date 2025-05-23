2359046faf6a47363868284b7c94f0362ee4ae8b
Vivek <c_vivek.patel@harness.io>
2025-05-23 18:30:35 +0530

    replace Dialog usages with ModalDialog

--- packages/ui/src/components/language-selector/language-dialog.tsx
+++ packages/ui/src/components/language-selector/language-dialog.tsx
@@ -1,8 +1,9 @@
 import { FC, useEffect, useState } from 'react'
 
-import { Dialog, Icon } from '@/components'
-import { cn } from '@utils/cn'
+import { Avatar } from '@components/avatar'
 
+import { CardSelect } from '../card-select'
+import { ModalDialog } from '../modal-dialog'
 import { Language, LanguageCode, LanguageDialogProps, LanguageInterface } from './types'
 
 export const languages: LanguageInterface[] = [
@@ -30,47 +31,37 @@ const LanguageDialog: FC<LanguageDialogProps> = ({
   }, [defaultLanguage, language])
 
   return (
-    <Dialog.Root open={open} onOpenChange={onOpenChange}>
-      {!!children && <Dialog.Trigger asChild>{children}</Dialog.Trigger>}
-      <Dialog.Content className="max-w-[400px]">
-        <Dialog.Title className="text-5 font-medium">Language</Dialog.Title>
-        <div className="mt-1 flex flex-col gap-3">
-          {supportedLanguages.map(lang => (
-            // TODO: Design system: Update language selector to Button component
-            <button
-              key={lang.code}
-              className="group relative flex cursor-pointer items-center justify-between rounded-md px-0 focus:ring-0 focus-visible:outline-none"
-              onClick={() => {
+    <ModalDialog.Root open={open} onOpenChange={onOpenChange}>
+      {!!children && <ModalDialog.Trigger asChild>{children}</ModalDialog.Trigger>}
+      <ModalDialog.Content className="max-w-[400px]">
+        <ModalDialog.Header>
+          <ModalDialog.Title className="text-5 font-medium">Language</ModalDialog.Title>
+        </ModalDialog.Header>
+        <ModalDialog.Body>
+          <CardSelect.Root
+            type="single"
+            value={selectedLanguage}
+            onValueChange={(val: unknown) => {
+              const code = val as LanguageCode
+              const lang = supportedLanguages.find(l => l.code === code)
+              if (lang) {
                 setSelectedLanguage(lang.code)
                 onChange(lang)
-              }}
-            >
-              <div className="relative z-[1] flex items-center gap-2">
-                <div className="flex size-6 items-center justify-center rounded bg-cn-background-12 text-1 uppercase text-cn-foreground-3">
-                  {lang.code}
+              }
+            }}
+          >
+            {supportedLanguages.map(lang => (
+              <CardSelect.Item key={lang.code} value={lang.code}>
+                <div className="flex items-center gap-2">
+                  <Avatar name={lang.code.split('').join(' ')} />
+                  <CardSelect.Title>{lang.name}</CardSelect.Title>
                 </div>
-                <span
-                  className={cn(
-                    'group-hover:text-cn-foreground-1',
-                    selectedLanguage === lang.code ? 'text-cn-foreground-1' : 'text-cn-foreground-2'
-                  )}
-                >
-                  {lang.name}
-                </span>
-              </div>
-              {selectedLanguage === lang.code && <Icon className="relative z-[1] text-icons-2" name="tick" size={12} />}
-              <span
-                className={cn(
-                  'absolute -inset-x-2 -inset-y-1 rounded -z-[1] group-hover:bg-cn-background-hover',
-                  selectedLanguage === lang.code && 'bg-cn-background-hover'
-                )}
-                aria-hidden
-              />
-            </button>
-          ))}
-        </div>
-      </Dialog.Content>
-    </Dialog.Root>
+              </CardSelect.Item>
+            ))}
+          </CardSelect.Root>
+        </ModalDialog.Body>
+      </ModalDialog.Content>
+    </ModalDialog.Root>
   )
 }
 

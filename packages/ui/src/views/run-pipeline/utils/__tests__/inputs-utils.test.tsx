90e435fc6d6cd5cedc7220628d53a4ff0bd8938f
Vardan Bansal <vardan.bansal@harness.io>
2025-06-10 11:50:29 -0700

    add unit tests

--- packages/ui/src/views/run-pipeline/utils/__tests__/inputs-utils.test.tsx
+++ packages/ui/src/views/run-pipeline/utils/__tests__/inputs-utils.test.tsx
@@ -1,11 +1,30 @@
+import label from 'tailwind-utils-config/components/label'
 import { describe, expect, it } from 'vitest'
 
 import { pipelineInputs2FormInputs } from '../inputs-utils'
 
 const pipelineInputs = {
-  foo: { type: 'string' },
-  bar: { type: 'string' },
-  baz: { type: 'string' }
+  foo: {
+    type: 'string',
+    label: 'Foo Input',
+    description: 'This is the foo input',
+    default: 'foo-default',
+    required: true
+  },
+  bar: {
+    type: 'string',
+    label: 'Bar Input',
+    description: 'This is the bar input',
+    default: 'bar-default',
+    required: false
+  },
+  baz: {
+    type: 'string',
+    label: 'Baz Input',
+    description: 'This is the baz input',
+    default: 'baz-default',
+    required: false
+  }
 }
 
 const options = { prefix: 'input.' }
@@ -28,13 +47,38 @@ describe('pipelineInputs2FormInputs', () => {
 
     // Assert group
     expect(result).toHaveLength(2)
-    expect(result[0].inputType).toBe('group')
-    expect(result[0].label).toBe('Main Group')
+    expect(result[0]).toMatchObject({
+      inputType: 'group',
+      path: '',
+      label: 'Main Group'
+    })
 
     // Assert unlisted input
     expect(result[1]).toMatchObject({
+      inputType: 'text',
       path: 'input.baz'
     })
+  })
+
+  it('includes grouped inputs with no label', () => {
+    const layout = [
+      {
+        open: true,
+        items: ['foo', { items: ['bar', 'baz'] }]
+      }
+    ]
+
+    const result = pipelineInputs2FormInputs({
+      pipelineInputs,
+      options,
+      pipelineInputLayout: layout
+    })
+
+    // Assert that all inputs are rendered as flat (not grouped) inputs
+    expect(result).toHaveLength(3)
+    expect(result[0].inputType).not.toBe('group')
+    expect(result[1].inputType).not.toBe('group')
+    expect(result[2].inputType).not.toBe('group')
   })
 
   it('includes duplicate keys in layout', () => {
@@ -53,6 +97,7 @@ describe('pipelineInputs2FormInputs', () => {
     })
 
     expect(result).toHaveLength(3)
+    // Assert that all inputs are rendered as flat (not grouped) inputs
     expect(result[0].inputType).not.toBe('group')
     expect(result[1].inputType).not.toBe('group')
     expect(result[2].inputType).not.toBe('group')

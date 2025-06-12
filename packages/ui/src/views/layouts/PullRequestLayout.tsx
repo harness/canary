7dce738b54e4119b894c96be5a094a440b30cdb5
Andrew Koreykin <ankormoreankor@gmail.com>
2025-06-12 22:17:47 +0400

    add tabs design migration

--- packages/ui/src/views/layouts/PullRequestLayout.tsx
+++ packages/ui/src/views/layouts/PullRequestLayout.tsx
@@ -1,24 +1,10 @@
-import { FC, PropsWithChildren, ReactNode } from 'react'
+import { FC } from 'react'
 
-import { CounterBadge, IconPropsV2, IconV2, TabNav } from '@/components'
+import { Tabs } from '@/components'
 import { useRouterContext, useTranslation } from '@/context'
 import { SandboxLayout } from '@views/layouts/SandboxLayout'
 import { PullRequestHeader } from '@views/repo/pull-request/components/pull-request-header'
 import { IPullRequestStore } from '@views/repo/pull-request/pull-request.types'
-
-const TabTitleWithIcon = ({
-  icon,
-  children,
-  badgeContent
-}: PropsWithChildren<{ icon: IconPropsV2['name']; badgeContent?: ReactNode }>) => (
-  <>
-    <div className="flex items-center gap-x-1">
-      <IconV2 className="text-icons-1 group-[.is-active]:text-icons-2" size="xs" name={icon} />
-      {children}
-    </div>
-    {!!badgeContent && <CounterBadge>{badgeContent}</CounterBadge>}
-  </>
-)
 
 interface PullRequestLayoutProps {
   usePullRequestStore: () => IPullRequestStore
@@ -50,28 +36,29 @@ export const PullRequestLayout: FC<PullRequestLayoutProps> = ({
           <PullRequestHeader className="mb-10" updateTitle={updateTitle} data={{ ...pullRequest, spaceId, repoId }} />
         )}
 
-        <TabNav.Root variant="tabs" className="mb-7 before:-left-6 before:w-[calc(100%+3rem)]">
-          <TabNav.Item to={PullRequestTabsKeys.CONVERSATION}>
-            <TabTitleWithIcon
+        <Tabs.NavRoot>
+          <Tabs.List className="-mx-6 mb-7 px-6" variant="overlined">
+            <Tabs.Trigger
+              value={PullRequestTabsKeys.CONVERSATION}
               icon="message"
-              badgeContent={!!pullRequest?.stats?.conversations && pullRequest?.stats?.conversations}
+              counter={pullRequest?.stats?.conversations}
             >
               {t('views:pullRequests.conversation', 'Conversation')}
-            </TabTitleWithIcon>
-          </TabNav.Item>
+            </Tabs.Trigger>
 
-          <TabNav.Item to={PullRequestTabsKeys.COMMITS}>
-            <TabTitleWithIcon icon="git-commit" badgeContent={pullRequest?.stats?.commits}>
+            <Tabs.Trigger value={PullRequestTabsKeys.COMMITS} icon="git-commit" counter={pullRequest?.stats?.commits}>
               {t('views:pullRequests.commits', 'Commits')}
-            </TabTitleWithIcon>
-          </TabNav.Item>
+            </Tabs.Trigger>
 
-          <TabNav.Item to={PullRequestTabsKeys.CHANGES}>
-            <TabTitleWithIcon icon="page-edit" badgeContent={pullRequest?.stats?.files_changed}>
+            <Tabs.Trigger
+              value={PullRequestTabsKeys.CHANGES}
+              counter={pullRequest?.stats?.files_changed}
+              icon="page-edit"
+            >
               {t('views:pullRequests.changes', 'Changes')}
-            </TabTitleWithIcon>
-          </TabNav.Item>
-        </TabNav.Root>
+            </Tabs.Trigger>
+          </Tabs.List>
+        </Tabs.NavRoot>
 
         <Outlet />
       </SandboxLayout.Content>

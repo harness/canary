03d16ebdfcf32ddcec5ea2263f836ffb70ba9e55
Vivek <c_vivek.patel@harness.io>
2025-06-12 16:25:55 +0530

    update gap token for button layout

--- packages/ui/tailwind-utils-config/components/button-layout.ts
+++ packages/ui/tailwind-utils-config/components/button-layout.ts
@@ -1,51 +1,51 @@
 export default {
-    '.cn-button-layout': {
-        gap: 'var(--cn-btn-wrapper-gap)',
-        '@apply flex': '',
+  '.cn-button-layout': {
+    gap: 'var(--cn-layout-sm)',
+    '@apply flex': '',
 
-        '&-vertical': {
-            '@apply flex-col': '',
+    '&-vertical': {
+      '@apply flex-col': '',
 
-            '.cn-button-layout-primary, .cn-button-layout-secondary': {
-                '@apply flex-col': '',
-            },
+      '.cn-button-layout-primary, .cn-button-layout-secondary': {
+        '@apply flex-col': ''
+      },
 
-            '.cn-button-layout-primary': {
-                '@apply order-1': ''
-            },
+      '.cn-button-layout-primary': {
+        '@apply order-1': ''
+      },
 
-            '.cn-button-layout-secondary': {
-                '@apply order-2': ''
-            }
+      '.cn-button-layout-secondary': {
+        '@apply order-2': ''
+      }
+    },
+
+    '&-horizontal': {
+      '&:where(.cn-button-layout-horizontal-end)': {
+        '@apply justify-end': '',
+
+        '.cn-button-layout-primary': {
+          '@apply order-2 ml-auto': ''
         },
 
-        '&-horizontal': {
-            '&:where(.cn-button-layout-horizontal-end)': {
-                '@apply justify-end': '',
+        '.cn-button-layout-secondary': {
+          '@apply order-first mr-auto': ''
+        }
+      },
 
-                '.cn-button-layout-primary': {
-                    '@apply order-2 ml-auto': ''
-                },
-
-                '.cn-button-layout-secondary': {
-                    '@apply order-first mr-auto': ''
-                }
-            },
-
-            '&:where(.cn-button-layout-horizontal-start)': {
-                '.cn-button-layout-primary': {
-                    '@apply order-first mr-auto': ''
-                },
-
-                '.cn-button-layout-secondary': {
-                    '@apply order-2 ml-auto': ''
-                }
-            }
+      '&:where(.cn-button-layout-horizontal-start)': {
+        '.cn-button-layout-primary': {
+          '@apply order-first mr-auto': ''
         },
 
-        '&-primary, &-secondary': {
-            gap: 'var(--cn-btn-wrapper-gap)',
-            '@apply flex': '',
-        },
+        '.cn-button-layout-secondary': {
+          '@apply order-2 ml-auto': ''
+        }
+      }
+    },
+
+    '&-primary, &-secondary': {
+      gap: 'var(--cn-layout-sm)',
+      '@apply flex': ''
     }
+  }
 }

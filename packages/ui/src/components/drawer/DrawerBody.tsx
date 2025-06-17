4baccf0053dec6b0d8c64fb25907003abc65ac45
Pavel Bublikov <pavel@pixelpoint.io>
2025-06-17 19:57:27 +0400

    fix: add scroll event handling section to ScrollArea documentation, fix drawer, cancel debounce

--- packages/ui/src/components/drawer/DrawerBody.tsx
+++ packages/ui/src/components/drawer/DrawerBody.tsx
@@ -4,6 +4,8 @@ import { ScrollArea } from '@/components'
 import { cn } from '@/utils'
 
 export const DrawerBody = ({ children, className }: PropsWithChildren<{ className?: string }>) => (
-  <ScrollArea className={cn('cn-drawer-body', className)}>{children}</ScrollArea>
+  <div className={cn('cn-drawer-body-wrap', className)}>
+    <ScrollArea className={cn('cn-drawer-body', className)}>{children}</ScrollArea>
+  </div>
 )
 DrawerBody.displayName = 'DrawerBody'

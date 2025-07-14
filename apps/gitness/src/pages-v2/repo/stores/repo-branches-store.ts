b1f19b24b1eee890dd475d11d04b605036353f1d
Vivek <c_vivek.patel@harness.io>
2025-07-14 20:24:33 +0530

    revert changes in repo-branches-store

--- apps/gitness/src/pages-v2/repo/stores/repo-branches-store.ts
+++ apps/gitness/src/pages-v2/repo/stores/repo-branches-store.ts
@@ -10,18 +10,26 @@ export const useRepoBranchesStore = create<IBranchSelectorStore>(set => ({
   tagList: [{ name: '', sha: '' }],
   branchList: [],
   selectedRefType: BranchSelectorTab.BRANCHES,
+  selectedBranchTag: null,
   xNextPage: 0,
   xPrevPage: 0,
   page: 1,
-  selectedBranchTag: null,
 
   // Actions
+  setSelectedBranchTag: (selectedBranchTag: BranchSelectorListItem) => set({ selectedBranchTag }),
   setSelectedRefType: (selectedRefType: BranchSelectorTab) => set({ selectedRefType }),
   setTagList: (tagList: BranchSelectorListItem[]) => set({ tagList }),
   setSpaceIdAndRepoId: (spaceId: string, repoId: string) => set({ spaceId, repoId }),
-  setBranchList: (branches: BranchData[]) => set({ branchList: branches }),
+  setBranchList: (branches: BranchData[]) =>
+    set({
+      branchList: branches
+    }),
+  setDefaultBranch: (branchName: string) =>
+    set({
+      defaultBranch: branchName
+    }),
   setPage: page => set({ page }),
-  setPaginationFromHeaders: (xNextPage: number, xPrevPage: number) => set({ xNextPage, xPrevPage }),
-  setSelectedBranchTag: (selectedBranchTag: BranchSelectorListItem) => set({ selectedBranchTag }),
-  setDefaultBranch: (branch: string) => set({ defaultBranch: branch })
+  setPaginationFromHeaders: (xNextPage: number, xPrevPage: number) => {
+    set({ xNextPage, xPrevPage })
+  }
 }))

998dc4330df6c365d282779ec3ae1ff06a3e25a7
Anastasia Ramina <131158394+cxramina@users.noreply.github.com>
2025-07-18 18:19:55 +0200

    Update tooltip.ts
    tooltip color updated

--- packages/ui/tailwind-utils-config/components/tooltip.ts
+++ packages/ui/tailwind-utils-config/components/tooltip.ts
@@ -3,10 +3,10 @@ export default {
     minWidth: 'var(--cn-tooltip-min)',
     maxWidth: 'var(--cn-tooltip-max)',
     borderRadius: 'var(--cn-tooltip-radius)',
-    border: 'var(--cn-tooltip-border) solid var(--cn-set-gray-solid-bg)',
-    background: 'var(--cn-set-gray-solid-bg)',
+    border: 'var(--cn-tooltip-border) solid var(--cn-border-2)',
+    background: 'var(--cn-bg-3)',
     padding: 'var(--cn-tooltip-py) var(--cn-tooltip-px)',
-    color: 'var(--cn-set-gray-solid-text)',
+    color: 'var(--cn-text-1)',
     boxShadow: 'var(--cn-shadow-2)',
     '@apply flex flex-col z-50 font-body-normal data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95':
       '',
@@ -16,7 +16,7 @@ export default {
     },
 
     '&-arrow': {
-      color: 'var(--cn-set-gray-solid-bg)',
+      color: 'var(--cn-bg-3)',
       '@apply w-5 h-2': ''
     }
   }

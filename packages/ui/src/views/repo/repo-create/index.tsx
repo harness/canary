70552395643ecf487bb86b276c2b9c9f08b3d56e
RadhaKrishna Dodla <c_radhakrishna.dodla@harness.io>
2025-07-21 19:35:43 -0700

    Prettier check

--- packages/ui/src/views/repo/repo-create/index.tsx
+++ packages/ui/src/views/repo/repo-create/index.tsx
@@ -115,7 +115,9 @@ export function RepoCreatePage({
     <SandboxLayout.Main>
       <SandboxLayout.Content className="mx-auto w-[570px] pb-20 pt-1">
         <Spacer size={5} />
-        <Text className="text-cn-foreground-1" variant="heading-section">{t('views:repos.createNewRepo', 'Create a new repository')}</Text>
+        <Text className="text-cn-foreground-1" variant="heading-section">
+          {t('views:repos.createNewRepo', 'Create a new repository')}
+        </Text>
         <Spacer size={2.5} />
         <Text className="max-w-[476px]">
           {t(

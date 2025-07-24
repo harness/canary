c66e60f0d32e48f7d853060a9f61d904c8728b7e
Vardan Bansal <vardan.bansal@harness.io>
2025-07-24 00:45:33 -0700

    always show top bar

--- packages/ui/src/views/repo/repo-list/repo-list-page.tsx
+++ packages/ui/src/views/repo/repo-list/repo-list-page.tsx
@@ -71,9 +71,6 @@ const SandboxRepoListPage: FC<RepoListProps> = ({
     )
   }
 
-  const noData = !(repositories && !!repositories.length)
-  const showTopBar = !noData || !!searchQuery?.length || page !== 1
-
   const handleResetFiltersQueryAndPages = () => {
     handleSearch('')
     setPage(1)
@@ -84,62 +81,60 @@ const SandboxRepoListPage: FC<RepoListProps> = ({
   return (
     <SandboxLayout.Main>
       <SandboxLayout.Content>
-        {showTopBar && (
-          <>
-            <Spacer size={8} />
-            <div className="flex items-end">
-              <Text variant="heading-section" as="h1">
-                {t('views:repos.repositories', 'Repositories')}
-              </Text>
-            </div>
-            <Spacer size={6} />
-            <FilterGroup<RepoListFilters, keyof RepoListFilters>
-              sortConfig={{
-                sortOptions: [],
-                onSortChange: noop
-              }}
-              onFilterValueChange={onFilterValueChange}
-              searchInput={searchQuery || ''}
-              handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
-              headerAction={
-                <SplitButton<string>
-                  dropdownContentClassName="mt-0 min-w-[170px]"
-                  handleButtonClick={() => navigate(toCreateRepo?.() || '')}
-                  handleOptionChange={option => {
-                    if (option === 'import') {
-                      navigate(toImportRepo?.() || '')
-                    } else if (option === 'import-multiple') {
-                      navigate(toImportMultipleRepos?.() || '')
-                    }
-                  }}
-                  options={[
-                    {
-                      value: 'import',
-                      label: t('views:repos.import-repository', 'Import Repository')
-                    },
-                    {
-                      value: 'import-multiple',
-                      label: t('views:repos.import-repositories', 'Import Repositories')
-                    }
-                  ]}
-                >
-                  {t('views:repos.create-repository', 'Create Repository')}
-                </SplitButton>
-              }
-              filterOptions={[
-                {
-                  label: t('views:connectors.filterOptions.statusOption.favorite', 'Favorites'),
-                  value: 'favorite',
-                  type: FilterFieldTypes.Checkbox,
-                  filterFieldConfig: {
-                    label: <IconV2 name="star-solid" size="md" className="text-cn-icon-yellow" />
+        <>
+          <Spacer size={8} />
+          <div className="flex items-end">
+            <Text variant="heading-section" as="h1">
+              {t('views:repos.repositories', 'Repositories')}
+            </Text>
+          </div>
+          <Spacer size={6} />
+          <FilterGroup<RepoListFilters, keyof RepoListFilters>
+            sortConfig={{
+              sortOptions: [],
+              onSortChange: noop
+            }}
+            onFilterValueChange={onFilterValueChange}
+            searchInput={searchQuery || ''}
+            handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
+            headerAction={
+              <SplitButton<string>
+                dropdownContentClassName="mt-0 min-w-[170px]"
+                handleButtonClick={() => navigate(toCreateRepo?.() || '')}
+                handleOptionChange={option => {
+                  if (option === 'import') {
+                    navigate(toImportRepo?.() || '')
+                  } else if (option === 'import-multiple') {
+                    navigate(toImportMultipleRepos?.() || '')
+                  }
+                }}
+                options={[
+                  {
+                    value: 'import',
+                    label: t('views:repos.import-repository', 'Import Repository')
                   },
-                  parser: booleanParser
-                }
-              ]}
-            />
-          </>
-        )}
+                  {
+                    value: 'import-multiple',
+                    label: t('views:repos.import-repositories', 'Import Repositories')
+                  }
+                ]}
+              >
+                {t('views:repos.create-repository', 'Create Repository')}
+              </SplitButton>
+            }
+            filterOptions={[
+              {
+                label: t('views:connectors.filterOptions.statusOption.favorite', 'Favorites'),
+                value: 'favorite',
+                type: FilterFieldTypes.Checkbox,
+                filterFieldConfig: {
+                  label: <IconV2 name="star-solid" size="md" className="text-cn-icon-yellow" />
+                },
+                parser: booleanParser
+              }
+            ]}
+          />
+        </>
         <Spacer size={5} />
         <RepoList
           repos={repositories || []}

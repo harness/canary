1dc8f426229814df9a9855286d018b137101512a
sanskar <c_sanskar.sehgal@harness.io>
2025-07-28 23:40:46 -0700

    feat: increase size of btns, align center

--- packages/ui/src/views/repo/components/branch-selector-v2/branch-selector.tsx
+++ packages/ui/src/views/repo/components/branch-selector-v2/branch-selector.tsx
@@ -52,11 +52,11 @@ export const BranchSelectorV2: FC<BranchSelectorProps> = ({
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
         {/* TODO: Design system: Add max-width from tailwind config and add .truncate to span */}
-        <Button className={cn('flex justify-start px-3', className)} variant="outline" size={buttonSize}>
+        <Button className={cn('justify-start px-3', className)} variant="outline" size={buttonSize}>
           {!branchPrefix && (
             <IconV2 className="shrink-0 fill-transparent" name={isTag ? 'tag' : 'git-branch'} size="xs" />
           )}
-          <Text className="w-[8.75rem] truncate text-left">
+          <Text className="truncate">
             {branchPrefix
               ? `${branchPrefix}: ${selectedBranch?.name || selectedBranchorTag.name}`
               : selectedBranch?.name || selectedBranchorTag.name}

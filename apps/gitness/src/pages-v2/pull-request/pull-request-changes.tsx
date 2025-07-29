fff0bead451bc092db380df8a0b1add132b2c0ee
shaurya-harness <shaurya.kalia@harness.io>
2025-07-29 19:36:07 +0530

    fix: PR panels checks and PR diff themeing

--- apps/gitness/src/pages-v2/pull-request/pull-request-changes.tsx
+++ apps/gitness/src/pages-v2/pull-request/pull-request-changes.tsx
@@ -33,6 +33,7 @@ import CommitSuggestionsDialog from '../../components-v2/commit-suggestions-dial
 import { useAppContext } from '../../framework/context/AppContext'
 import { useGetRepoRef } from '../../framework/hooks/useGetRepoPath'
 import { useIsMFE } from '../../framework/hooks/useIsMFE'
+import { useMFEContext } from '../../framework/hooks/useMFEContext'
 import { useQueryState } from '../../framework/hooks/useQueryState'
 import { PathParams } from '../../RouteDefinitions'
 import { filenameToLanguage, normalizeGitRef } from '../../utils/git-utils'
@@ -86,6 +87,10 @@ export default function PullRequestChanges() {
   const [searchPrincipalsQuery, setSearchPrincipalsQuery] = useState('')
   const [jumpToDiff, setJumpToDiff] = useState('')
   const isMfe = useIsMFE()
+
+  const {
+    scope: { accountId }
+  } = useMFEContext()
 
   const {
     data: { body: principals } = {},

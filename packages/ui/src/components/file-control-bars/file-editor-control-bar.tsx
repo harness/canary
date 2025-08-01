40b2140f510f40f05b34baf2ffbe8f5fa2e8ad36
Alex <zemka4@gmail.com>
2025-08-02 00:03:07 +0300

    Align tab Files code edit view with design (#2037)
    * Align tab Files code edit view with design
    
    * Align tab Files code edit view with design
    
    * Align tab Files code edit view with design
    
    * Align tab Files code edit view with design
    
    * Align tab Files code edit view with design
    
    * Align tab Files code edit view with design

--- packages/ui/src/components/file-control-bars/file-editor-control-bar.tsx
+++ packages/ui/src/components/file-control-bars/file-editor-control-bar.tsx
@@ -1,27 +1,13 @@
-import { FC } from 'react'
+import { StackedList, Tabs } from '@/components'
 
-import { EditViewTypeValue, StackedList, ToggleGroup } from '@/components'
-
-export interface FileEditorControlBarProps {
-  view: EditViewTypeValue
-  onChangeView: (value: EditViewTypeValue) => void
-}
-
-export const FileEditorControlBar: FC<FileEditorControlBarProps> = ({ view, onChangeView }) => {
+export const FileEditorControlBar = () => {
   return (
-    <StackedList.Root onlyTopRounded borderBackground>
-      <StackedList.Item disableHover isHeader className="px-4 py-1.5">
-        <ToggleGroup.Root
-          onChange={onChangeView as (value: string) => void}
-          value={view}
-          unselectable
-          size="xs"
-          variant="ghost"
-          selectedVariant="secondary"
-        >
-          <ToggleGroup.Item value={'edit'} text="Edit" />
-          <ToggleGroup.Item value={'preview'} text="Preview" />
-        </ToggleGroup.Root>
+    <StackedList.Root className="bg-cn-background-2" onlyTopRounded>
+      <StackedList.Item disableHover isHeader className="px-cn-md py-cn-2xs">
+        <Tabs.List variant="ghost">
+          <Tabs.Trigger value="edit">Edit</Tabs.Trigger>
+          <Tabs.Trigger value="preview">Preview</Tabs.Trigger>
+        </Tabs.List>
       </StackedList.Item>
     </StackedList.Root>
   )

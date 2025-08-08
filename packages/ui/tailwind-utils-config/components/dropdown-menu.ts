1d0dc7fc0ea2e9943d59310e37b2e4e9ce201262
Alex <zemka4@gmail.com>
2025-08-08 16:33:46 +0300

    Fix dropdown content height on small screen heights (#2059)

--- packages/ui/tailwind-utils-config/components/dropdown-menu.ts
+++ packages/ui/tailwind-utils-config/components/dropdown-menu.ts
@@ -38,7 +38,7 @@ export default {
     borderRadius: 'var(--cn-dropdown-radius)',
     backgroundColor: 'var(--cn-bg-3)',
     boxShadow: 'var(--cn-shadow-4)',
-    '@apply data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2':
+    '@apply flex flex-col data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2':
       '',
 
     '&-container': {

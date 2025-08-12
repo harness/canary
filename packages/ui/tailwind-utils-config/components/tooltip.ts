e832455e4c5f2290b7fdf35ee70ca77aa8b17da0
Anastasia Ramina <131158394+cxramina@users.noreply.github.com>
2025-08-12 21:44:31 +0200

    Update tooltip.ts
    toltip color fixes based on the updated light mode

--- packages/ui/tailwind-utils-config/components/tooltip.ts
+++ packages/ui/tailwind-utils-config/components/tooltip.ts
@@ -3,11 +3,10 @@ export default {
     minWidth: 'var(--cn-tooltip-min)',
     maxWidth: 'var(--cn-tooltip-max)',
     borderRadius: 'var(--cn-tooltip-radius)',
-    border: 'var(--cn-tooltip-border) solid var(--cn-set-gray-soft-bg)',
-    background: 'var(--cn-set-gray-soft-bg)',
+    border: 'var(--cn-tooltip-border) solid var(--cn-set-gray-solid-bg)',
+    background: 'var(--cn-set-gray-solid-bg)',
     padding: 'var(--cn-tooltip-py) var(--cn-tooltip-px)',
-    color: 'var(--cn-set-gray-soft-text)',
-    boxShadow: 'var(--cn-shadow-2)',
+    color: 'var(--cn-set-gray-solid-text)',
     '@apply flex flex-col z-50 font-body-normal data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95':
       '',
 
@@ -16,7 +15,7 @@ export default {
     },
 
     '&-arrow': {
-      color: 'var(--cn-set-gray-soft-bg)',
+      color: 'var(--cn-set-gray-solid-bg)',
       '@apply w-5 h-2': ''
     }
   }

feab024a3cf8862b5b18bfb610f3465ad39711af
Anastasia Ramina <131158394+cxramina@users.noreply.github.com>
2025-08-14 14:30:06 +0200

    Update alert-dialog.tsx
    Update alert-dialog.tsx
    
    Update alert-dialog.tsx
    
    Update alert-dialog.tsx

--- packages/ui/src/components/alert-dialog.tsx
+++ packages/ui/src/components/alert-dialog.tsx
@@ -78,15 +78,12 @@ const Content = ({ title, children }: ContentProps) => {
 
   return (
     <Dialog.Content onOpenAutoFocus={event => event.preventDefault()}>
-     <Dialog.Header
-  icon={
-    <IconV2
-      name={context.theme === 'danger' ? 'xmark-circle' : context.theme === 'warning' ? 'warning-triangle' : undefined}
-      size="xl"
-    />
-  }
-  theme={context.theme}
->
+    <Dialog.Header
+        icon={
+          context.theme === 'danger' ? 'xmark-circle' : context.theme === 'warning' ? 'warning-triangle' : undefined
+        }
+        theme={context.theme}
+      >
         <Dialog.Title>{title}</Dialog.Title>
       </Dialog.Header>
 

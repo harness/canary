085c3f83262dceeb10146d92d5695cd5e9da0a33
Vardan Bansal <vardan.bansal@harness.io>
2025-08-20 16:10:50 -0700

    cleanup repo list mfe

--- apps/gitness/src/pages-v2/repo/repo-list-mfe.tsx
+++ apps/gitness/src/pages-v2/repo/repo-list-mfe.tsx
@@ -6,12 +6,9 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { Button, SplitButton } from '@harnessio/ui/components'
 import { ComponentProvider, PortalProvider } from '@harnessio/ui/context'
 
-import { ShadowRootLoader } from '../../AppMFE'
-import ShadowRootWrapper from '../../components-v2/shadow-root-wrapper'
 import { AppRouterProvider } from '../../framework/context/AppRouterProvider'
 import { NavigationProvider } from '../../framework/context/NavigationContext'
 import { queryClient } from '../../framework/queryClient'
-import { useLoadMFEStyles } from '../../hooks/useLoadMFEStyles'
 import ReposListPage from './repo-list'
 
 const RepoListMFE = ({ renderUrl }: { renderUrl: string }) => {
@@ -20,7 +17,7 @@ const RepoListMFE = ({ renderUrl }: { renderUrl: string }) => {
 
   const routes: RouteObject[] = [
     {
-      path: 'repos',
+      path: '/',
       element: (
         <AppRouterProvider>
           <ComponentProvider components={{ RbacButton: Button, RbacSplitButton: SplitButton }}>
@@ -31,39 +28,20 @@ const RepoListMFE = ({ renderUrl }: { renderUrl: string }) => {
     }
   ]
 
-  const theme = 'Light'
-
   const router = createBrowserRouter(routes, { basename })
-
-  // Styles will be loaded inside shadowRoot
-  const shadowRef = useRef<HTMLDivElement>(null)
-  const shadowRoot = shadowRef.current?.shadowRoot
 
   // Radix UI elements will be rendered inside portalContainer
   const portalRef = useRef<HTMLDivElement>(null)
   const portalContainer = portalRef.current
 
-  const isStylesLoaded = useLoadMFEStyles(shadowRoot) || true
   return (
-    <div ref={shadowRef} id="repo-list-mfe-root">
-      <ShadowRootWrapper>
-        {/* Radix UI elements need to be rendered inside the following div with the theme class */}
-        <div className={theme.toLowerCase()} ref={portalRef}>
-          {!isStylesLoaded ? (
-            // Replace it with spinner once it is available
-            <ShadowRootLoader theme={theme} />
-          ) : (
-            <PortalProvider portalContainer={portalContainer}>
-              <QueryClientProvider client={queryClient}>
-                <NavigationProvider routes={routes}>
-                  <RouterProvider router={router} />
-                </NavigationProvider>
-              </QueryClientProvider>
-            </PortalProvider>
-          )}
-        </div>
-      </ShadowRootWrapper>
-    </div>
+    <PortalProvider portalContainer={portalContainer}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationProvider routes={routes}>
+          <RouterProvider router={router} />
+        </NavigationProvider>
+      </QueryClientProvider>
+    </PortalProvider>
   )
 }
 

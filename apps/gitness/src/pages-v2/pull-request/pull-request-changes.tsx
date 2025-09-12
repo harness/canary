6e74bc60ee33f83b306aed11a1aea2e28bae0aad
Vardan Bansal <vardan.bansal@harness.io>
2025-09-12 17:37:35 +0000

    fix: Handle "usePreferenceStore" missing from MFE context (#10432)
    * 73a015 minor revert for compatibility
    
    * 0e16da handle undefined

--- apps/gitness/src/pages-v2/pull-request/pull-request-changes.tsx
+++ apps/gitness/src/pages-v2/pull-request/pull-request-changes.tsx
@@ -81,6 +81,7 @@ export default function PullRequestChanges() {
     () => pullReqCommits?.commits?.map(commit => commit.sha as string),
     [pullReqCommits?.commits]
   )
+  const [diffMode, setDiffMode] = useState<DiffModeEnum>(DiffModeEnum.Split)
   const targetRef = useMemo(() => pullReqMetadata?.merge_base_sha, [pullReqMetadata?.merge_base_sha])
   const sourceRef = useMemo(() => pullReqMetadata?.source_sha, [pullReqMetadata?.source_sha])
   const prId = (pullRequestId && Number(pullRequestId)) || -1
@@ -92,11 +93,8 @@ export default function PullRequestChanges() {
   const isMfe = useIsMFE()
 
   const {
-    scope: { accountId },
-    customHooks: { usePreferenceStore }
+    scope: { accountId }
   } = useMFEContext()
-
-  const { preference: diffMode, setPreference: setDiffMode } = usePreferenceStore('USER', 'diffMode')
 
   const {
     data: { body: principals } = {},
@@ -607,7 +605,7 @@ export default function PullRequestChanges() {
         usePullRequestProviderStore={usePullRequestProviderStore}
         setDiffMode={setDiffMode}
         loadingReviewers={loadingReviewers}
-        diffMode={diffMode || DiffModeEnum.Split}
+        diffMode={diffMode}
         reviewers={reviewers}
         refetchReviewers={refetchReviewers}
         submitReview={submitReview}

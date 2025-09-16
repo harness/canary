c6aa0f06fbb1a746d89a97f2fc2c746bc592f0dd
Pavel Bublikov <pavel@pixelpoint.io>
2025-09-16 17:18:27 +0400

    feat: enhance secrets list UI

--- packages/ui/src/views/secrets/secrets-list/types.ts
+++ packages/ui/src/views/secrets/secrets-list/types.ts
@@ -27,6 +27,9 @@ export interface SecretListProps extends Partial<RoutingProps> {
   isLoading: boolean
   onEditSecret: (secret: SecretListItem) => void
   onDeleteSecret: (secretId: string) => void
+  handleResetFiltersQueryAndPages?: () => void
+  isDirtyList: boolean
+  onCreateSecret?: () => void
 }
 
 export interface SecretListPageProps extends SecretListProps {

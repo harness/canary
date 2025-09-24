d2aef77a26a7fe06b54f403b90d487276191486a
RadhaKrishna Dodla <c_radhakrishna.dodla@harness.io>
2025-09-24 16:21:19 -0700

    Added Tag filter for Secrets

--- packages/ui/src/components/filters/types.ts
+++ packages/ui/src/components/filters/types.ts
@@ -19,6 +19,7 @@ export type SecretListFilters = {
   secretTypes?: CheckboxOptions[]
   secretManagerIdentifiers?: CheckboxOptions[]
   description?: string
+  tags?: string
 }
 
 export interface CheckboxOptions {

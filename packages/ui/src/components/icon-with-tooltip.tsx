21eef5bb70f8068776268da3866e8222068033d3
Pranesh TG <pranesh.g@harness.io>
2025-09-26 11:26:43 +0000

    Update default size of the IconWithTooltip to md (#10474)
    * eca651 Update default size of the IconWithTooltip to md

--- packages/ui/src/components/icon-with-tooltip.tsx
+++ packages/ui/src/components/icon-with-tooltip.tsx
@@ -15,7 +15,7 @@ export interface IconWithTooltipProps extends Omit<TooltipProps, 'children'> {
 export const IconWithTooltip = ({ className, disabled, iconProps, ...props }: IconWithTooltipProps) => (
   <Tooltip {...props}>
     <button className={cn({ 'pointer-events-none': disabled }, className)} disabled={disabled}>
-      <IconV2 name="info-circle" {...(iconProps as PropsWithoutRef<IconPropsV2>)} />
+      <IconV2 name="info-circle" size="md" {...(iconProps as PropsWithoutRef<IconPropsV2>)} />
     </button>
   </Tooltip>
 )

9e70c2b6eda093349398f7733205c0d92ff22f89
Andrew Koreykin <ankormoreankor@gmail.com>
2025-10-01 19:11:17 +0400

    fixes

--- packages/ui/src/views/repo/components/branch-banner/branch-compare-banner.tsx
+++ packages/ui/src/views/repo/components/branch-banner/branch-compare-banner.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 
-import { Button, IconV2, Layout, Text, TimeAgoCard } from '@/components'
+import { Button, IconV2, Layout, Link, Text, TimeAgoCard } from '@/components'
 import { useRouterContext, useTranslation } from '@/context'
 import { TypesBranchTable } from '@/views'
 
@@ -18,28 +18,29 @@ interface BranchCompareBannerProps {
 
 const BranchCompareBanner: FC<BranchCompareBannerProps> = ({ branch, defaultBranchName, repoId, spaceId }) => {
   const { t } = useTranslation()
-  const { Link } = useRouterContext()
+  const { Link: RouterLink } = useRouterContext()
 
   return (
     <Layout.Flex justify="between" align="center" gap="sm">
       <Layout.Grid gap="2xs" align="center" flow="column">
         <IconV2 name="git-branch" size="sm" color="success" />
-        <Text truncate>
-          <Link to={`${spaceId ? `/${spaceId}` : ''}/repos/${repoId}/summary/refs/heads/${branch.name}`}>
-            {branch.name}
-          </Link>
-        </Text>
+        <Link
+          to={`${spaceId ? `/${spaceId}` : ''}/repos/${repoId}/summary/refs/heads/${branch.name}`}
+          className="font-body-strong text-cn-1 inline-block w-auto truncate"
+        >
+          {branch.name}
+        </Link>
         <Text wrap="nowrap">
           {t('views:repos.hadRecentPushes', 'had recent pushes ')}
           <TimeAgoCard timestamp={branch.updated} textProps={{ color: 'foreground-2', truncate: true }} />
         </Text>
       </Layout.Grid>
       <Button variant="primary" theme="success" asChild>
-        <Link
+        <RouterLink
           to={`${spaceId ? `/${spaceId}` : ''}/repos/${repoId}/pulls/compare/${defaultBranchName}...${branch.name}`}
         >
           {t('views:repos.compareAndPullRequest', 'Compare & pull request')}
-        </Link>
+        </RouterLink>
       </Button>
     </Layout.Flex>
   )

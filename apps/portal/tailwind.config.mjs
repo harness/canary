48bd645a6f01a6370d80d9f51fc4268a9ec13eab
harish-viswa <harish.viswanathan@harness.io>
2025-10-06 10:10:56 +0530

    feat: updated review comments

--- apps/portal/tailwind.config.mjs
+++ apps/portal/tailwind.config.mjs
@@ -1,12 +1,15 @@
 import starlightPlugin from "@astrojs/starlight-tailwind";
-import tailwindConfig from "@harnessio/ui/tailwind.config";
+import { uiTailwindPreset } from "@harnessio/ui/tailwind.config";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  presets: [tailwindConfig],
-  content: [
-    "node_modules/@harnessio/ui/src/**/*.{ts,tsx}",
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  presets: [
+    uiTailwindPreset([
+      "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    ]),
   ],
   plugins: [starlightPlugin(), require("tailwindcss-animate")],
+  corePlugins: {
+    preflight: true,
+  },
 };

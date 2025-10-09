dd2d949a810c06e62ca0f976b57c64fb5360312b
Srdjan Arsic <srdjan.arsic@harness.io>
2025-10-09 16:00:04 +0200

    warning support for step template

--- packages/ui/src/views/run-pipeline/types.ts
+++ packages/ui/src/views/run-pipeline/types.ts
@@ -57,6 +57,7 @@ export interface PipelineInput {
 
 export interface PipelineInputDefinition extends PipelineInput {
   ui?: InputUIConfig
+  warning?: string
 }
 
 export interface InputUIConfig {

dfc0f8c2cf2ca22aef1ce90928f9f596e5633cec
Alex <zemka4@gmail.com>
2025-10-29 12:54:22 +0300

    Fix Secrets list table (#2219)

--- apps/design-system/src/pages/view-preview/view-preview.tsx
+++ apps/design-system/src/pages/view-preview/view-preview.tsx
@@ -97,7 +97,7 @@ export const viewPreviews: Record<string, ViewPreviewGroup> = {
         element: (
           <SideNavWrapper>
             <Routes>
-              <Route path="home" element={<div className="p-cn-xl">Home</div>} />
+              <Route path="home" element={<SecretsListPage />} />
               <Route path="activity" element={<div className="p-cn-xl">Activity</div>} />
               {/* other routes */}
             </Routes>

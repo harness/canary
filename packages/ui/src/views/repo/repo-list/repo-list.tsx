01414c769f49ad2c18f3b44bec3a7d587f6f2dc8
Alex <zemka4@gmail.com>
2025-10-29 06:54:32 +0300

    Fix repo list: empty states (hide filters and pagination), fix search placeholder (#2238)
    * Fix repo list: empty states (hide filters and pagination), fix search placeholder
    
    * Fix repo list: empty states (hide filters and pagination), fix search placeholder

--- packages/ui/src/views/repo/repo-list/repo-list.tsx
+++ packages/ui/src/views/repo/repo-list/repo-list.tsx
@@ -109,6 +109,7 @@ export function RepoList({
       />
     ) : (
       <NoData
+        withBorder
         imageName="no-repository"
         title={t('views:noData.noRepos', 'No repositories yet')}
         description={[

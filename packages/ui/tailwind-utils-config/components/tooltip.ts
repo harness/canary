59e08ad63e6990c033fc287129269e9709efc35b
cx.ramina <anastasiia.ramina@harness.io>
2025-11-04 14:48:15 +0100

    added shadow to the tooltip

--- packages/ui/tailwind-utils-config/components/tooltip.ts
+++ packages/ui/tailwind-utils-config/components/tooltip.ts
@@ -8,6 +8,7 @@ export default {
     background: 'var(--cn-bg-3)',
     padding: 'var(--cn-tooltip-py) var(--cn-tooltip-px)',
     color: 'var(--cn-text-1)',
+    boxShadow: 'var(--cn-shadow-2)',
     '@apply overflow-auto flex flex-col z-50 font-body-normal data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95':
       '',
 

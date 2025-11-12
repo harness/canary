d5c0e98d3887400a1a4a1d2edd4fa8b4434d9748
Pranesh <pranesh.g@harness.io>
2025-11-12 16:57:10 +0530

    feat: [UUI-226]: Update Meter component

--- packages/ui/src/components/meter.tsx
+++ packages/ui/src/components/meter.tsx
@@ -1,25 +1,34 @@
+import { ReactNode } from 'react'
+
 import { cn } from '@utils/cn'
+
+import { Tooltip, TooltipProvider } from './tooltip'
 
 export enum MeterState {
   Empty = 0,
   Error = 1,
   Warning = 2,
-  Success = 3
+  Success = 3,
+  Info = 4
 }
 
 interface IMeterProps {
   data?: {
     id?: string
     state: MeterState
+    tooltip?: {
+      content: ReactNode
+    }
   }[]
   className?: string
 }
 
 const stateToBgColor: { [key in MeterState]: string } = {
-  [MeterState.Empty]: 'bg-cn-1/20',
+  [MeterState.Empty]: 'bg-cn-gray-secondary',
   [MeterState.Error]: 'bg-cn-danger-primary',
   [MeterState.Warning]: 'bg-cn-warning-primary',
-  [MeterState.Success]: 'bg-cn-success-primary'
+  [MeterState.Success]: 'bg-cn-success-primary',
+  [MeterState.Info]: 'bg-cn-brand-primary'
 }
 
 function Meter({ data = [], className }: IMeterProps) {
@@ -27,12 +36,24 @@ function Meter({ data = [], className }: IMeterProps) {
   const bars: IMeterProps['data'] = [...Array(emptyBarsCount).fill({ state: MeterState.Empty }), ...data]
 
   return (
-    <div className={cn('flex h-[19px] items-stretch gap-cn-3xs', className)}>
-      {bars.map(col => {
-        const bgColor = stateToBgColor[col.state as MeterState]
-        return <div key={col.id} className={cn('flex h-full w-[5px] rounded-cn-px', bgColor)} />
-      })}
-    </div>
+    <TooltipProvider skipDelayDuration={0} delayDuration={0}>
+      <div className={cn('cn-meter', className)}>
+        {bars.map((col, index) => {
+          const bgColor = stateToBgColor[col.state as MeterState]
+          const tooltip = col.tooltip
+          const key = col.id || `meter-bar-${index}`
+
+          if (tooltip) {
+            return (
+              <Tooltip key={key} content={tooltip.content} delay={0}>
+                <div className={cn('cn-meter-bar cn-meter-tooltip-bar', bgColor)} />
+              </Tooltip>
+            )
+          }
+          return <div key={key} className={cn('cn-meter-bar', bgColor)} />
+        })}
+      </div>
+    </TooltipProvider>
   )
 }
 

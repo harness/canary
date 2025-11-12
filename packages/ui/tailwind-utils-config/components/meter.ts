9e7154ff75cdf16470d57290addfca1080c67cf2
Pranesh <pranesh.g@harness.io>
2025-11-12 16:57:10 +0530

    feat: [UUI-226]: Update Meter component

---
+++ packages/ui/tailwind-utils-config/components/meter.ts
@@ -0,0 +1,18 @@
+export default {
+  '.cn-meter': {
+    '@apply flex h-[19px] items-stretch gap-cn-3xs': '',
+
+    '&:has(.cn-meter-tooltip-bar:hover) > .cn-meter-tooltip-bar:not(:hover)': {
+      opacity: '0.3'
+    },
+
+    '&:has(.cn-meter-tooltip-bar:hover) > .cn-meter-tooltip-bar:hover': {
+      transform: 'scaleY(1.2)'
+    },
+
+    '&-bar': {
+      '@apply flex h-full w-[5px] rounded-px': '',
+      transition: 'all 0.15s'
+    }
+  }
+}

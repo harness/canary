f95100cbb98b140dc5db111d890dc718626dec81
Avinash Madhwani <avinashmadhwani02@gmail.com>
2025-11-18 13:58:16 +0530

    updated vite config

--- packages/ai-chat-core/vite.config.ts
+++ packages/ai-chat-core/vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+
 import pkg from './package.json'
 
 const external = [...new Set([...Object.keys(pkg.devDependencies || []), ...Object.keys(pkg.peerDependencies || [])])]

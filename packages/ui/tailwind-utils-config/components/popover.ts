1cfcd289a48073cf0ebb74143e4549c9dc98747b
cx.ramina <anastasiia.ramina@harness.io>
2025-12-02 18:19:31 +0100

    popover styles update
    popover styles update
    
    popover styles update
    
    popover styles update

--- packages/ui/tailwind-utils-config/components/popover.ts
+++ packages/ui/tailwind-utils-config/components/popover.ts
@@ -7,9 +7,9 @@ export default {
       padding: 'var(--cn-popover-py) var(--cn-popover-px)',
       gap: 'var(--cn-popover-gap)',
       borderRadius: 'var(--cn-popover-radius)',
-      border: 'var(--cn-popover-border) solid var(--cn-border-3)',
+      border: 'var(--cn-popover-border) solid var(--cn-border-2)',
       backgroundColor: 'var(--cn-bg-3)',
-      boxShadow: 'var(--cn-shadow-2)',
+      boxShadow: 'var(--cn-shadow-3)',
       color: 'var(--cn-text-1)',
       outline: 'none',
       '@apply z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95':
@@ -22,7 +22,7 @@ export default {
 
     '&-arrow': {
       color: 'var(--cn-bg-3)',
-      stroke: 'var(--cn-border-3)',
+      stroke: 'var(--cn-border-2)',
       '@apply w-5 h-2': ''
     }
   }

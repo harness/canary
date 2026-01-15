3b58565a4d1bbac455a8d5dccbd2a5f5cb616618
Radhakrishna Dodla <c_radhakrishna.dodla@harness.io>
2026-01-15 15:10:48 +0000

    Add gap prop to StatsPanel component (#10784)
    * 2de8c7 Add gap prop to StatsPanel component

--- packages/ui/src/components/stats-panel.tsx
+++ packages/ui/src/components/stats-panel.tsx
@@ -2,18 +2,21 @@ import { FC } from 'react'
 
 import { Layout, Skeleton, Text } from '@/components'
 
+type GapSize = 'none' | '4xs' | '3xs' | '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl'
+
 export interface StatsPanelProps {
   data: { label: string; value?: JSX.Element }[]
   isLoading?: boolean
+  gap?: GapSize
 }
 
-export const StatsPanel: FC<StatsPanelProps> = ({ data, isLoading = false }) => {
+export const StatsPanel: FC<StatsPanelProps> = ({ data, isLoading = false, gap = 'sm' }) => {
   if (!data.length) return null
 
   return (
     <Layout.Flex wrap="wrap" gap="3xl">
       {data.map((stat, index) => (
-        <Layout.Vertical gap="sm" key={index}>
+        <Layout.Vertical gap={gap} key={index}>
           <Text color="foreground-3">{stat.label}</Text>
           {isLoading ? (
             <Skeleton.Typography className="w-16" />

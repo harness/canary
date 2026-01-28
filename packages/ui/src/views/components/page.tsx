30a84fa14561f6d1caf2d6aede6ebef2eb31780d
Shaurya Kalia <shaurya.kalia@harness.io>
2026-01-28 21:11:15 +0000

    fix: page header spacing alignments (#10867)
    * 952f7e fix: page header spacing alignments

--- packages/ui/src/views/components/page.tsx
+++ packages/ui/src/views/components/page.tsx
@@ -65,7 +65,7 @@ const Header: FC<PageHeaderProps> = ({
     )
 
   return (
-    <Layout.Vertical gap="md" className={cn('w-full', children ? 'mb-cn-md' : 'mb-cn-3xl')}>
+    <Layout.Vertical gap="md" className={cn('w-full', children ? 'mb-cn-md' : 'mb-cn-2xl')}>
       <Layout.Horizontal className="h-cn-9">
         {!!backLink && (
           <Link prefixIcon {...backLink.linkProps}>

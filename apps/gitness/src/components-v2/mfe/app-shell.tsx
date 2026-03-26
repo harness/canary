d02be0fd15da694196c30cc989d61c7e68a12573
rkapoor10 <ritik.kapoor@harness.io>
2026-03-26 22:56:13 +0530

    feat: [UUI-849] use platform ui headers

--- apps/gitness/src/components-v2/mfe/app-shell.tsx
+++ apps/gitness/src/components-v2/mfe/app-shell.tsx
@@ -5,18 +5,23 @@ import { Toaster } from '@harnessio/ui/components'
 import { MainContentLayout } from '@harnessio/views'
 
 import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
+import { useIsUnifiedExperience } from '../../framework/hooks/useIsUnifiedExperience'
 import useSpaceSSEWithPubSub from '../../framework/hooks/useSpaceSSEWithPubSub'
 
 export const AppShellMFE = memo(() => {
   const spaceURL = useGetSpaceURLParam()
+  const isUnifiedExperience = useIsUnifiedExperience()
 
   useSpaceSSEWithPubSub({
     space: spaceURL ?? ''
   })
 
+  const showBreadcrumbs = !isUnifiedExperience && breadcrumbs.length > 0
+
   return (
     <>
-      <MainContentLayout className="text-cn-2" withBreadcrumbs={false}>
+      {showBreadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
+      <MainContentLayout className="text-cn-2" withBreadcrumbs={showBreadcrumbs}>
         <Outlet />
       </MainContentLayout>
       <Toaster />

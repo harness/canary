a944f4bf42156aac2b3293be11174c85154f9e61
Radhakrishna Dodla <c_radhakrishna.dodla@harness.io>
2026-03-31 17:11:35 +0000

    feat: [UUI-849]: New breadcrumbs and remove dedicated breadcrumbs for Code V2 (#11034)
    * 8a61c6 feat: [UUI-849]:new breadcrumbs and remove dedicated breadcrumbs for Code V2

--- apps/gitness/src/components-v2/mfe/app-shell.tsx
+++ apps/gitness/src/components-v2/mfe/app-shell.tsx
@@ -6,11 +6,8 @@ import { MainContentLayout } from '@harnessio/views'
 
 import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
 import useSpaceSSEWithPubSub from '../../framework/hooks/useSpaceSSEWithPubSub'
-import { Breadcrumbs } from '../breadcrumbs/breadcrumbs'
-import { useGetBreadcrumbs } from '../breadcrumbs/useGetBreadcrumbs'
 
 export const AppShellMFE = memo(() => {
-  const { breadcrumbs } = useGetBreadcrumbs()
   const spaceURL = useGetSpaceURLParam()
 
   useSpaceSSEWithPubSub({
@@ -19,8 +16,7 @@ export const AppShellMFE = memo(() => {
 
   return (
     <>
-      <Breadcrumbs breadcrumbs={breadcrumbs} />
-      <MainContentLayout className="text-cn-2" withBreadcrumbs={breadcrumbs.length > 0}>
+      <MainContentLayout className="text-cn-2" withBreadcrumbs={false}>
         <Outlet />
       </MainContentLayout>
       <Toaster />

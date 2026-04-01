7a08c170ca5907c697758b086a1cad8730f289c3
Radhakrishna Dodla <c_radhakrishna.dodla@harness.io>
2026-04-01 19:36:13 +0000

    fix: [UUI-943]: Code breadcrumbs should be hide under FF (#11040)
    * b6de48 prettier fix
    
    * 3bdc0b Unit tests for breadcrumb hide
    
    * 9eecb9 fix: []: Code breadcrumbs should be hide under FF

--- apps/gitness/src/components-v2/mfe/app-shell.tsx
+++ apps/gitness/src/components-v2/mfe/app-shell.tsx
@@ -4,11 +4,23 @@ import { Outlet } from 'react-router-dom'
 import { Toaster } from '@harnessio/ui/components'
 import { MainContentLayout } from '@harnessio/views'
 
+import { FeatureFlag } from '../../framework/context/MFEContext'
 import { useGetSpaceURLParam } from '../../framework/hooks/useGetSpaceParam'
+import { useMFEContext } from '../../framework/hooks/useMFEContext'
 import useSpaceSSEWithPubSub from '../../framework/hooks/useSpaceSSEWithPubSub'
+import { Breadcrumbs } from '../breadcrumbs/breadcrumbs'
+import { useGetBreadcrumbs } from '../breadcrumbs/useGetBreadcrumbs'
 
 export const AppShellMFE = memo(() => {
+  const { customHooks } = useMFEContext()
+  const { breadcrumbs } = useGetBreadcrumbs()
   const spaceURL = useGetSpaceURLParam()
+  const flags = (customHooks?.useFeatureFlags?.() ?? {}) as Record<string, unknown>
+  const unifiedExperience =
+    typeof window !== 'undefined' && window.localStorage.getItem('enable_unified_experience') === 'true'
+  const hideLegacyMfeBreadcrumbs =
+    Boolean(flags[FeatureFlag.PL_ENABLE_CANARY_UI]) ||
+    (Boolean(flags[FeatureFlag.PL_UNIFIED_OPT_IN_ENABLED]) && unifiedExperience)
 
   useSpaceSSEWithPubSub({
     space: spaceURL ?? ''
@@ -16,7 +28,8 @@ export const AppShellMFE = memo(() => {
 
   return (
     <>
-      <MainContentLayout className="text-cn-2" withBreadcrumbs={false}>
+      {!hideLegacyMfeBreadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
+      <MainContentLayout className="text-cn-2" withBreadcrumbs={!hideLegacyMfeBreadcrumbs && breadcrumbs.length > 0}>
         <Outlet />
       </MainContentLayout>
       <Toaster />

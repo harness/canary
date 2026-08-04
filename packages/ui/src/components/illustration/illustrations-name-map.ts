5651570027026c90c33f657599a1fd43189abe86
Aaron Palmer <aaron.palmer@harness.io>
2026-08-04 15:23:58 +0000

    feat: [UUI-2191]: Update empty state illustrations (#11235)
    * 90397a Tokenize empty-state illustrations with cn CSS variables, consolidate to single theme-aware files
    
    * 8c670a Fix prettier
    
    * bd40e8 Optimize empty-state illustration SVGs with SVGO
    
    * 253f34 Update empty-state illustrations to new cube design, add 5 new illustrations

--- packages/ui/src/components/illustration/illustrations-name-map.ts
+++ packages/ui/src/components/illustration/illustrations-name-map.ts
@@ -7,35 +7,25 @@ import CreateWorkspaceLight from './images/create-workspace-light.svg'
 // Import standard images
 import CreateWorkspace from './images/create-workspace.svg'
 import HarnessLogoText from './images/harness-logo-text.svg'
-import NoDataBranchesLight from './images/no-data-branches-light.svg'
 import NoDataBranches from './images/no-data-branches.svg'
-import NoDataCogLight from './images/no-data-cog-light.svg'
 import NoDataCog from './images/no-data-cog.svg'
-import NoDataCommitsLight from './images/no-data-commits-light.svg'
 import NoDataCommits from './images/no-data-commits.svg'
-import NoDataDelegateLight from './images/no-data-delegate-light.svg'
 import NoDataDelegate from './images/no-data-delegate.svg'
-import NoDataErrorLight from './images/no-data-error-light.svg'
 import NoDataError from './images/no-data-error.svg'
-import NoDataFolderLight from './images/no-data-folder-light.svg'
 import NoDataFolder from './images/no-data-folder.svg'
-import NoDataLabelsLight from './images/no-data-labels-light.svg'
 import NoDataLabels from './images/no-data-labels.svg'
-import NoDataMembersLight from './images/no-data-members-light.svg'
 import NoDataMembers from './images/no-data-members.svg'
-import NoDataMergeLight from './images/no-data-merge-light.svg'
 import NoDataMerge from './images/no-data-merge.svg'
-import NoDataMetricsLight from './images/no-data-metrics-light.svg'
 import NoDataMetrics from './images/no-data-metrics.svg'
-import NoDataPRLight from './images/no-data-pr-light.svg'
+import NoDataNotifications from './images/no-data-notifications.svg'
+import NoDataPipeline from './images/no-data-pipeline.svg'
 import NoDataPR from './images/no-data-pr.svg'
-import NoRepositoryLight from './images/no-data-repository-light.svg'
+import NoDataReference from './images/no-data-reference.svg'
 import NoRepository from './images/no-data-repository.svg'
-import NoDataTagsLight from './images/no-data-tags-light.svg'
+import NoDataSecrets from './images/no-data-secrets.svg'
+import NoDataSecurity from './images/no-data-security.svg'
 import NoDataTags from './images/no-data-tags.svg'
-import NoDataWebhooksLight from './images/no-data-webhooks-light.svg'
 import NoDataWebhooks from './images/no-data-webhooks.svg'
-import NoSearchMagnifyingGlassLight from './images/no-search-magnifying-glass-light.svg'
 import NoSearchMagnifyingGlass from './images/no-search-magnifying-glass.svg'
 import SubMenuEllipse from './images/sub-menu-ellipse.svg'
 import TiSavingsCardLight from './images/ti-savings-card-light.svg'
@@ -66,6 +56,11 @@ export const IllustrationsNameMap = {
   'no-data-labels': NoDataLabels,
   'no-data-webhooks': NoDataWebhooks,
   'no-data-metrics': NoDataMetrics,
+  'no-data-notifications': NoDataNotifications,
+  'no-data-pipeline': NoDataPipeline,
+  'no-data-reference': NoDataReference,
+  'no-data-secrets': NoDataSecrets,
+  'no-data-security': NoDataSecurity,
   'no-search-magnifying-glass': NoSearchMagnifyingGlass,
   'no-delegates': NoDataDelegate,
   'harness-logo-text': HarnessLogoText,
@@ -85,21 +80,6 @@ export const IllustrationsNameMap = {
   // Light theme variants
   'chat-avatar-light': ChatAvatarLight,
   'create-workspace-light': CreateWorkspaceLight,
-  'no-data-branches-light': NoDataBranchesLight,
-  'no-data-cog-light': NoDataCogLight,
-  'no-data-commits-light': NoDataCommitsLight,
-  'no-data-error-light': NoDataErrorLight,
-  'no-data-metrics-light': NoDataMetricsLight,
-  'no-data-folder-light': NoDataFolderLight,
-  'no-data-members-light': NoDataMembersLight,
-  'no-data-merge-light': NoDataMergeLight,
-  'no-data-pr-light': NoDataPRLight,
-  'no-repository-light': NoRepositoryLight,
-  'no-data-tags-light': NoDataTagsLight,
-  'no-data-labels-light': NoDataLabelsLight,
-  'no-data-webhooks-light': NoDataWebhooksLight,
-  'no-delegates-light': NoDataDelegateLight,
-  'no-search-magnifying-glass-light': NoSearchMagnifyingGlassLight,
   'ti-savings-card-light': TiSavingsCardLight,
   'welcome-light': WelcomeLight
 } as const satisfies Record<string, React.FunctionComponent<React.SVGProps<SVGSVGElement>>>

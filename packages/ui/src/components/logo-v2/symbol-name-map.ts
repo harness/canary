3f52d2dc7cd40b95d5017bec96f1ece2ee82d1d4
Jyoti Arora <jyoti.arora@harness.io>
2026-08-14 06:26:03 +0000

    feat: [CCM-34996]: updated icons (#11313)
    * 4bde68 feat: CCM-34996: updated icons

--- packages/ui/src/components/logo-v2/symbol-name-map.ts
+++ packages/ui/src/components/logo-v2/symbol-name-map.ts
@@ -4,6 +4,7 @@
  */
 
 import AiVerify from './symbols/ai-verify.svg'
+import Alpine from './symbols/alpine.svg'
 import Anchore from './symbols/anchore.svg'
 import Android from './symbols/android.svg'
 import AppDynamics from './symbols/app-dynamics.svg'
@@ -28,23 +29,30 @@ import Blackduck from './symbols/blackduck.svg'
 import Brakeman from './symbols/brakeman.svg'
 import BuildKite from './symbols/build-kite.svg'
 import Burb from './symbols/burb.svg'
+import Chainguard from './symbols/chainguard.svg'
 import ChatGpt from './symbols/chat-gpt.svg'
 import Checkmarx from './symbols/checkmarx.svg'
 import Checkov from './symbols/checkov.svg'
 import Claude from './symbols/claude.svg'
 import Codecov from './symbols/codecov.svg'
+import Composer from './symbols/composer.svg'
+import Conan from './symbols/conan.svg'
+import Conda from './symbols/conda.svg'
 import Confluence from './symbols/confluence.svg'
 import Connector from './symbols/connector.svg'
+import Cran from './symbols/cran.svg'
 import Cursor from './symbols/cursor.svg'
 import CustomDeployment from './symbols/custom-deployment.svg'
 import CustomIngest from './symbols/custom-ingest.svg'
 import CustomScan from './symbols/custom-scan.svg'
 import CustomSecretManager from './symbols/custom-secret-manager.svg'
+import Dart from './symbols/dart.svg'
 import DataDog from './symbols/data-dog.svg'
 import DatabaseApplySchema from './symbols/database-apply-schema.svg'
 import DatabaseCheck from './symbols/database-check.svg'
 import DatabaseRollback from './symbols/database-rollback.svg'
 import Databricks from './symbols/databricks.svg'
+import Debian from './symbols/debian.svg'
 import DefaultSecretManager from './symbols/default-secret-manager.svg'
 import Docker from './symbols/docker.svg'
 import Dynatrace from './symbols/dynatrace.svg'
@@ -71,6 +79,7 @@ import Harness from './symbols/harness.svg'
 import Hashicorp from './symbols/hashicorp.svg'
 import Helm from './symbols/helm.svg'
 import Hubspot from './symbols/hubspot.svg'
+import Huggingface from './symbols/huggingface.svg'
 import Java from './symbols/java.svg'
 import JenkinsX from './symbols/jenkins-x.svg'
 import Jenkins from './symbols/jenkins.svg'
@@ -82,6 +91,7 @@ import Kustomize from './symbols/kustomize.svg'
 import Linux from './symbols/linux.svg'
 import Liquibase from './symbols/liquibase.svg'
 import Local from './symbols/local.svg'
+import Maven from './symbols/maven.svg'
 import McpConnector from './symbols/mcp-connector.svg'
 import Mcp from './symbols/mcp.svg'
 import Mend from './symbols/mend.svg'
@@ -96,6 +106,7 @@ import Nikto from './symbols/nikto.svg'
 import Nmap from './symbols/nmap.svg'
 import NodeJs from './symbols/node-js.svg'
 import Npm from './symbols/npm.svg'
+import Nuget from './symbols/nuget.svg'
 import Oci from './symbols/oci.svg'
 import OpenShift from './symbols/open-shift.svg'
 import OpenTofu from './symbols/open-tofu.svg'
@@ -108,14 +119,18 @@ import Playwright from './symbols/playwright.svg'
 import PrismaCloud from './symbols/prisma-cloud.svg'
 import Prometheus from './symbols/prometheus.svg'
 import Prowler from './symbols/prowler.svg'
+import Puppet from './symbols/puppet.svg'
 import Python from './symbols/python.svg'
 import Qtest from './symbols/qtest.svg'
 import Qualys from './symbols/qualys.svg'
 import Queue from './symbols/queue.svg'
 import Qwiet from './symbols/qwiet.svg'
 import Rancher from './symbols/rancher.svg'
+import Redhat from './symbols/redhat.svg'
+import Ruby from './symbols/ruby.svg'
 import RunTest from './symbols/run-test.svg'
 import Run from './symbols/run.svg'
+import Rust from './symbols/rust.svg'
 import Salesforce from './symbols/salesforce.svg'
 import SecretSsh from './symbols/secret-ssh.svg'
 import SecureShell from './symbols/secure-shell.svg'
@@ -133,6 +148,7 @@ import SqlRollback from './symbols/sql-rollback.svg'
 import Sql from './symbols/sql.svg'
 import SscaOrchestrate from './symbols/ssca-orchestrate.svg'
 import SumoLogic from './symbols/sumo-logic.svg'
+import Swift from './symbols/swift.svg'
 import Sysdig from './symbols/sysdig.svg'
 import Tanzu from './symbols/tanzu.svg'
 import TasVars from './symbols/tas-vars.svg'
@@ -152,6 +168,7 @@ import Zoom from './symbols/zoom.svg'
 
 export const SymbolNameMap = {
   'ai-verify': AiVerify,
+  alpine: Alpine,
   anchore: Anchore,
   android: Android,
   'app-dynamics': AppDynamics,
@@ -176,23 +193,30 @@ export const SymbolNameMap = {
   brakeman: Brakeman,
   'build-kite': BuildKite,
   burb: Burb,
+  chainguard: Chainguard,
   'chat-gpt': ChatGpt,
   checkmarx: Checkmarx,
   checkov: Checkov,
   claude: Claude,
   codecov: Codecov,
+  composer: Composer,
+  conan: Conan,
+  conda: Conda,
   confluence: Confluence,
   connector: Connector,
+  cran: Cran,
   cursor: Cursor,
   'custom-deployment': CustomDeployment,
   'custom-ingest': CustomIngest,
   'custom-scan': CustomScan,
   'custom-secret-manager': CustomSecretManager,
+  dart: Dart,
   'data-dog': DataDog,
   'database-apply-schema': DatabaseApplySchema,
   'database-check': DatabaseCheck,
   'database-rollback': DatabaseRollback,
   databricks: Databricks,
+  debian: Debian,
   'default-secret-manager': DefaultSecretManager,
   docker: Docker,
   dynatrace: Dynatrace,
@@ -219,6 +243,7 @@ export const SymbolNameMap = {
   hashicorp: Hashicorp,
   helm: Helm,
   hubspot: Hubspot,
+  huggingface: Huggingface,
   java: Java,
   'jenkins-x': JenkinsX,
   jenkins: Jenkins,
@@ -230,6 +255,7 @@ export const SymbolNameMap = {
   linux: Linux,
   liquibase: Liquibase,
   local: Local,
+  maven: Maven,
   'mcp-connector': McpConnector,
   mcp: Mcp,
   mend: Mend,
@@ -244,6 +270,7 @@ export const SymbolNameMap = {
   nmap: Nmap,
   'node-js': NodeJs,
   npm: Npm,
+  nuget: Nuget,
   oci: Oci,
   'open-shift': OpenShift,
   'open-tofu': OpenTofu,
@@ -256,14 +283,18 @@ export const SymbolNameMap = {
   'prisma-cloud': PrismaCloud,
   prometheus: Prometheus,
   prowler: Prowler,
+  puppet: Puppet,
   python: Python,
   qtest: Qtest,
   qualys: Qualys,
   queue: Queue,
   qwiet: Qwiet,
   rancher: Rancher,
+  redhat: Redhat,
+  ruby: Ruby,
   'run-test': RunTest,
   run: Run,
+  rust: Rust,
   salesforce: Salesforce,
   'secret-ssh': SecretSsh,
   'secure-shell': SecureShell,
@@ -281,6 +312,7 @@ export const SymbolNameMap = {
   sql: Sql,
   'ssca-orchestrate': SscaOrchestrate,
   'sumo-logic': SumoLogic,
+  swift: Swift,
   sysdig: Sysdig,
   tanzu: Tanzu,
   'tas-vars': TasVars,

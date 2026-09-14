aa105305c1086ba4d8b01296340dea5b5748af20
Katie Grantier <katie.grantier@harness.io>
2026-09-14 11:03:14 -0700

    Update calendar component sidebar label to be more descriptive
    Co-Authored-By: Figma <noreply@figma.com>

--- apps/portal/astro.config.ts
+++ apps/portal/astro.config.ts
@@ -178,7 +178,7 @@ export default defineConfig({
               items: [
                 {
                   slug: "components/form/calendar",
-                  label: "Overview",
+                  label: "Calendar",
                 },
                 {
                   slug: "components/form/calendar/calendar-input",
